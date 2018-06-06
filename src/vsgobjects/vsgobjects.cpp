dd2ff1ca9aed66ead33ba48e0c21e903ef848aa8
Robert Osfield <robert@openscenegraph.com>
2018-06-06 19:09:27 +0100

    Restructed the sizeof tests to make it clearer

--- src/vsgobjects/vsgobjects.cpp
+++ src/vsgobjects/vsgobjects.cpp
@@ -7,6 +7,8 @@
 #include <osg/Referenced>
 #include <osg/Group>
 #include <osg/Timer>
+#include <osg/Observer>
+#include <osg/UserDataContainer>
 
 #include <iostream>
 #include <vector>
@@ -18,27 +20,10 @@ vsg::Group* createGroup()
     return group.release();
 }
 
-class OsgClass : public osg::Referenced
-{
-public:
-
-    OsgClass()
-    {
-        std::cout<<"OsgClass::OsgClass() "<<this<<std::endl;
-    }
-
-protected:
-    virtual ~OsgClass()
-    {
-        std::cout<<"OsgClass::~OsgClass() "<<this<<std::endl;
-    }
-};
-
-
 int main(int argc, char** argv)
 {
-    auto start = std::chrono::high_resolution_clock::now(); // will eventually need to use chrono::steady_clock for frame stats
 
+    auto start = std::chrono::high_resolution_clock::now(); // will eventually need to use chrono::steady_clock for frame stats
     std::cout<<"-- Before"<<std::endl<<std::endl;
     vsg::ref_ptr<vsg::Object> global;
     {
@@ -60,14 +45,8 @@ int main(int argc, char** argv)
         group->setObject(vsg::Object::Key("list",5), new vsg::Object());
 
 
-        vsg::ref_ptr<OsgClass> osg = new OsgClass;
+        //vsg::ref_ptr<OsgClass> osg = new OsgClass;
 
-        std::cout<<"size_of<OsgClass> "<<sizeof(OsgClass)<<std::endl;
-        std::cout<<"size_of<osg::Node> "<<sizeof(osg::Node)<<std::endl;
-        std::cout<<"size_of<vsg::Node> "<<sizeof(vsg::Node)<<std::endl;
-        std::cout<<"size_of<osg::Grouo> "<<sizeof(osg::Group)<<std::endl;
-        std::cout<<"size_of<vsg::Grouo> "<<sizeof(vsg::Group)<<std::endl;
-        std::cout<<"size_of<vsg::ObjectConnections> "<<sizeof(vsg::ObjectConnections)<<std::endl;
 
         global = group;
         std::cout<<"---- End of block"<<std::endl<<std::endl;
@@ -76,10 +55,21 @@ int main(int argc, char** argv)
     std::cout<<" global->getObject(\"userdata\") = "<<global->getObject("userdata")<<std::endl;
 
     global = nullptr;
-
     auto end = std::chrono::high_resolution_clock::now();
     double std_duration = std::chrono::duration<double>(end-start).count();
     std::cout<<"-- After, elapsed time "<<std_duration<<std::endl;
+
+    std::cout<<std::endl;
+    std::cout<<"size_of<osg::Object> "<<sizeof(osg::Object)<<std::endl;
+    std::cout<<"size_of<osg::Node> "<<sizeof(osg::Node)<<std::endl;
+    std::cout<<"size_of<osg::Grouo> "<<sizeof(osg::Group)<<std::endl;
+    std::cout<<"size_of<osg::ObserverSet> "<<sizeof(osg::ObserverSet)<<std::endl;
+    std::cout<<"size_of<osg::UserDataContainer> "<<sizeof(osg::UserDataContainer)<<std::endl;
+    std::cout<<std::endl;
+    std::cout<<"size_of<vsg::Object> "<<sizeof(vsg::Object)<<std::endl;
+    std::cout<<"size_of<vsg::Node> "<<sizeof(vsg::Node)<<std::endl;
+    std::cout<<"size_of<vsg::Grouo> "<<sizeof(vsg::Group)<<std::endl;
+    std::cout<<"size_of<vsg::ObjectConnections> "<<sizeof(vsg::ObjectConnections)<<std::endl;
 
     return 0;
 }
