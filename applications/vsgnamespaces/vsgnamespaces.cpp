18028dae2178b12b60344b3bd0f0ad82063f7f58
Robert Osfield <robert@openscenegraph.com>
2018-06-11 10:05:37 +0100

    Added first pass at namesapce and classes

--- applications/vsgnamespaces/vsgnamespaces.cpp
+++ applications/vsgnamespaces/vsgnamespaces.cpp
@@ -1,33 +1,110 @@
-#include <vsg/Object>
+#include <vsg/Object.h>
+#include <vsg/ref_ptr.h>
 
+// preliminary list of compoents the the vsg libraries and the possible classes that they'll contain
+// listed here just to give an overall feel of what will be needed
 namespace vsg
 {
     namespace core
     {
-        class CoreClass : public vsg::Object
-        {
-        };
+        class ref_ptr;
+        class objsever_ptr;
+        class Object;
+        class Auxiliary;
+        class Visitor;
     }
 
     namespace maths
     {
-        class mat4
-        {
-        };
+        class vec2; // double, float, int, uint etc. variants
+        class vec3;
+        class vec4;
+        class mat4;
+        // GLSL style functions like cross, dot, inverse etc.
     }
 
-    namespace viewwer
+    namespace nodes
     {
-        class Viewer : public vsg::Object
-        {
-        };
+        class Node;
+        class Group;
+        class Cull;
+        class LOD;
+        class PagedLOD;
+        class Transform;
+        class Mask;
+        class Switch;
+        class State; // attach PipelineComponents to the scene graph
+        class Draw; // dispatch a graphics call
+        class Compute; // dispatch a compute call
+        // need some scene graph mechansism for controlling draw order/conrolling the rendering backend
+    };
 
-        class View : public vsg::Object
-        {
-        };
+    namespace state
+    {
+        class PipelineComponent;
+        class PipelineComponents; // collection of PipelinComponents
+        class PipelineState; // complete set of PipelineConponents to be passed to GL
+        class Uniform; // pipline component
+        class Array; // pipline component
+        class Texture; // pipline component
+        class Buffer; // pipline component
+        class Program; // pipline component
+        class Shader; // pipline component
+        class Define; // some sort of #pragma(tic) shader composition
+    };
+
+    namespace render
+    {
+        class RenderBin;  // has map<PipelineState*, std::vector<Draw/Compute> and std::vector<pair<PipelineState*, Draw/Compute> for ordered list.
+        class RenderStage;
+        class StateGraph;
+        class CullTraversal; // traverse scene graph to generate rendering graphs
+        class DrawTraversal; // traverse rendering graphs to dispatch to vulkan
     }
+
+    namespace compute
+    {
+        // ?  Need to research what Vulkan does for compute
+    }
+
+    namespace viewer
+    {
+        class Camera; // also a Node, perhaps should be in nodes?
+        class View;
+        class Viewer; // "has a" list of View
+        class RenderSurface; // context/buffer object?
+        class Compositor; // presentor, take all the rendering output and present on screen
+        // some basic event handling?
+    }
+
+    namespace introspection
+    {
+        // some sort of generic API for classes, properties and methods and global functions. make available to C?
+        // some sort of object wrapping
+    }
+
+    namespace io
+    {
+        ref_ptr<Object> readFile(std::iostream&);
+        ref_ptr<Object> readFile(const std::string& filename);
+        void writeFile(Object& object, std::ostream&);
+        void writeFile(Object& object, const std::string& filename);
+    }
+
 };
 
+namespace vsg
+{
+    // for convinience we want all the compoents to be available in the vsg namespace
+    using namespace core;
+    using namespace nodes;
+    using namespace state;
+    using namespace render;
+    using namespace compute;
+    using namespace viewer;
+    using namespace introspection;
+    using namespace io;
+}
 
 int main(int argc, char** argv)
 {
