2ad245d0082e00869ae51fb17ef68a60163a17e7
Robert Osfield <robert@openscenegraph.com>
2018-06-12 09:55:36 +0100

    Added template FixedGroup<int N> template that uses a local std::array for children. Added using QuadGroup = FixedGroup<4>

---
+++ include/vsg/nodes/FixedGroup.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vsg/core/ref_ptr.h>
+
+#include <vsg/nodes/Node.h>
+
+#include <array>
+#include <algorithm>
+
+namespace vsg
+{
+    template<int N>
+    class FixedGroup : public vsg::Node
+    {
+    public:
+        FixedGroup() {}
+
+        virtual void accept(Visitor& visitor) { visitor.apply(*this); }
+
+        inline virtual void traverse(Visitor& visitor)
+        {
+            std::for_each(_children.begin(), _children.end(), [&visitor](ref_ptr<Node>& child)
+            {
+                if (child.valid()) child->accept(visitor);
+            });
+        }
+
+        void setChild(std::size_t pos, vsg::Node* node)
+        {
+            _children[pos] = node;
+        }
+
+        vsg::Node* getChild(std::size_t pos) { return _children[pos].get(); }
+        const vsg::Node* getChild(std::size_t pos) const { return _children[pos].get(); }
+
+        std::size_t getNumChildren() const { return N; }
+
+        using Children = std::array< ref_ptr< vsg::Node>, N >;
+
+        Children& getChildren() { return _children; }
+        const Children& getChildren() const { return _children; }
+
+    protected:
+
+        virtual ~FixedGroup() {}
+
+        Children _children;
+    };
+
+    using QuadGroup = FixedGroup<4>;
+
+}
