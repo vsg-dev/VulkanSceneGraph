5fc7afa325daf55a489f276644573cde3e70dbbe
Robert Osfield <robert@openscenegraph.com>
2018-06-15 14:32:28 +0100

    Added vec*.h and mat4.h headers Added quick test of mat4 and Object::setValue/getValue(.., mat4)

--- applications/vsgmaths/vsgmaths.cpp
+++ applications/vsgmaths/vsgmaths.cpp
@@ -2,14 +2,17 @@
 #include <vsg/core/observer_ptr.h>
 #include <vsg/core/Object.h>
 #include <vsg/core/Auxiliary.h>
+#include <vsg/core/Visitor.h>
 
 #include <vsg/maths/vec2.h>
 #include <vsg/maths/vec3.h>
 #include <vsg/maths/vec4.h>
+#include <vsg/maths/mat4.h>
 
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <cstddef>
 
 
 ////////////////////////////////////////////////////////////////////
@@ -34,6 +37,16 @@ template<typename T>
 inline std::ostream& operator << (std::ostream& output, const vsg::tvec4<T>& vec)
 {
     output << vec.x << " " << vec.y<<" "<<vec.z<<" "<<vec.w;
+    return output; // to enable cascading
+}
+
+template<typename T>
+inline std::ostream& operator << (std::ostream& output, const vsg::tmat4<T>& vec)
+{
+    output << vec(0,0)<< " " << vec(0,1)<<" "<<vec(0,2)<<" "<<vec(0,3)<<std::endl;
+    output << vec(1,0)<< " " << vec(1,1)<<" "<<vec(1,2)<<" "<<vec(1,3)<<std::endl;
+    output << vec(2,0)<< " " << vec(2,1)<<" "<<vec(2,2)<<" "<<vec(2,3)<<std::endl;
+    output << vec(3,0)<< " " << vec(3,1)<<" "<<vec(3,2)<<" "<<vec(3,3)<<std::endl;
     return output; // to enable cascading
 }
 
@@ -64,6 +77,22 @@ int main(int argc, char** argv)
     vsg::vec4d colour(1.0, 0.9, 1.0, 0.5);
     std::cout<<"colour = ("<<colour<<")"<<std::endl;
 
+    vsg::mat4 mat;
+    mat(3,0) = 102.3;
+    std::cout<<"mat = "<<mat<<std::endl;
+
+    vsg::tmat4<short> cmat;
+    std::cout<<"cmat = "<<cmat<<std::endl;
+    std::cout<<"sizeof(cmat) = "<<sizeof(cmat)<<std::endl;
+
+    vsg::ref_ptr<vsg::Object> object = new vsg::Object;
+    object->setValue("matrix", mat);
+
+    vsg::mat4 new_mat;
+    if (object->getValue("matrix", new_mat))
+    {
+        std::cout<<"getValue(\"matrix\""<<new_mat<<std::endl;
+    }
 
     return 0;
 }
