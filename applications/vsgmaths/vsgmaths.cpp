e469877a92a0f2f9ba9869b5eae4bd30ea8b0751
Robert Osfield <robert@openscenegraph.com>
2018-06-15 09:23:55 +0100

    Added first cut of tvec3<> and associated vec3, vec3f, vec4d.

--- applications/vsgmaths/vsgmaths.cpp
+++ applications/vsgmaths/vsgmaths.cpp
@@ -16,9 +16,6 @@ struct tvec2
 {
     using value_type = T;
 
-    tvec2() : data{T(), T()} {}
-    tvec2(T x, T y) : data{x, y} {}
-
     union
     {
         value_type  data[2];
@@ -26,6 +23,9 @@ struct tvec2
         struct { value_type r, g; };
         struct { value_type s, t; };
     };
+
+    tvec2() : data{value_type(), value_type()} {}
+    tvec2(value_type x, value_type y) : data{x, y} {}
 
     std::size_t size() const { return 2; }
 
@@ -38,9 +38,42 @@ using vec2d = tvec2<double>;
 using vec2 = vec2f;
 
 template<typename T>
+struct tvec3
+{
+    using value_type = T;
+
+    union
+    {
+        value_type  data[3];
+        struct { value_type x, y, z; };
+        struct { value_type r, g, a; };
+        struct { value_type s, t, p; };
+    };
+
+    tvec3() : data{value_type(), value_type(), value_type()} {}
+    tvec3(value_type x, value_type y, value_type z) : data{x, y, z} {}
+
+    std::size_t size() const { return 3; }
+
+    value_type & operator[] (std::size_t i) { return data[i]; }
+    value_type operator[] (std::size_t i) const { return data[i]; }
+};
+
+using vec3f = tvec3<float>;
+using vec3d = tvec3<double>;
+using vec3 = vec3f;
+
+template<typename T>
 inline std::ostream& operator << (std::ostream& output, const tvec2<T>& vec)
 {
     output << vec.x << " " << vec.y;
+    return output; // to enable cascading
+}
+
+template<typename T>
+inline std::ostream& operator << (std::ostream& output, const tvec3<T>& vec)
+{
+    output << vec.x << " " << vec.y<<" "<<vec.z;
     return output; // to enable cascading
 }
 
@@ -59,8 +92,8 @@ int main(int argc, char** argv)
     std::cout<<"vec2.data="<<v.data<<" ("<<v.data[0]<<", "<<v.data[1]<<")"<<std::endl;
     std::cout<<"vec2[0]=("<<v[0]<<", "<<v[1]<<")"<<std::endl;
 
-    vec2d n(2.0, 1.0);
-    std::cout<<"n(x="<<n.x<<", y="<<n.y<<")"<<std::endl;
+    vec3d n(2.0, 1.0, 0.5);
+    std::cout<<"n(x="<<n.x<<", y="<<n.y<<", z="<<n.z<<")"<<std::endl;
 
     std::cout<<"n = "<<n<<std::endl;
 
