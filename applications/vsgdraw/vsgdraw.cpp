da4ed926f7c8655742f3ff870a4595e085069b8d
Robert Osfield <robert@openscenegraph.com>
2018-06-28 13:50:20 +0100

    Added set up of graphics pipeline

--- applications/vsgdraw/vsgdraw.cpp
+++ applications/vsgdraw/vsgdraw.cpp
@@ -42,6 +42,8 @@ namespace vsg
     public:
         PipelineLayout(Device* device, VkPipelineLayout pipelineLayout, VkAllocationCallbacks* pAllocator=nullptr);
 
+        PipelineLayout(Device* device, VkAllocationCallbacks* pAllocator=nullptr);
+
         operator VkPipelineLayout () const { return _pipelineLayout; }
 
     protected:
@@ -52,7 +54,20 @@ namespace vsg
         VkAllocationCallbacks*  _pAllocator;
     };
 
+    class Pipeline : public vsg::Object
+    {
+    public:
+        Pipeline(Device* device, VkPipeline pipeline, VkAllocationCallbacks* pAllocator=nullptr);
 
+        operator VkPipeline () const { return _pipeline; }
+
+    protected:
+        virtual ~Pipeline();
+
+        ref_ptr<Device>         _device;
+        VkPipeline              _pipeline;
+        VkAllocationCallbacks*  _pAllocator;
+    };
 }
 
 // implementation
@@ -66,6 +81,22 @@ PipelineLayout::PipelineLayout(Device* device, VkPipelineLayout pipelineLayout, 
 {
 }
 
+PipelineLayout::PipelineLayout(Device* device, VkAllocationCallbacks* pAllocator) :
+    _device(device),
+    _pipelineLayout(VK_NULL_HANDLE),
+    _pAllocator(pAllocator)
+{
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount = 0;
+    pipelineLayoutInfo.pushConstantRangeCount = 0;
+
+    if (vkCreatePipelineLayout(*device, &pipelineLayoutInfo, pAllocator, &_pipelineLayout) != VK_SUCCESS)
+    {
+        std::cout<<"Failed to create VkPipelineLayout"<<std::endl;
+    }
+}
+
 PipelineLayout::~PipelineLayout()
 {
     if (_pipelineLayout)
@@ -75,6 +106,128 @@ PipelineLayout::~PipelineLayout()
     }
 }
 
+Pipeline::Pipeline(Device* device, VkPipeline pipeline, VkAllocationCallbacks* pAllocator) :
+    _device(device),
+    _pipeline(pipeline),
+    _pAllocator(pAllocator)
+{
+}
+
+Pipeline::~Pipeline()
+{
+    if (_pipeline)
+    {
+        std::cout<<"Calling vkDestroyPipeline"<<std::endl;
+        vkDestroyPipeline(*_device, _pipeline, _pAllocator);
+    }
+}
+
+ref_ptr<Pipeline> createGraphicsPipeline(Device* device, Swapchain* swapchain, RenderPass* renderPass, PipelineLayout* pipelineLayout, ShaderModule* vert, ShaderModule* frag, VkAllocationCallbacks* pAllocator=nullptr)
+{
+    VkPipelineShaderStageCreateInfo vertexShaderStageInfo = {};
+    vertexShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertexShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertexShaderStageInfo.module = *vert;
+    vertexShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragmentShaderStageInfo = {};
+    fragmentShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragmentShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragmentShaderStageInfo.module = *frag;
+    fragmentShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = {vertexShaderStageInfo, fragmentShaderStageInfo};
+
+    // vertex attrobite input
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo = {};
+    vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertexInputInfo.vertexBindingDescriptionCount = 0;
+    vertexInputInfo.vertexAttributeDescriptionCount = 0;
+
+    // primitive input
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly = {};
+    inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+    // set up viewport
+    const VkExtent2D& extent = swapchain->getExtent();
+
+    VkViewport viewport = {};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = static_cast<float>(extent.width);
+    viewport.height = static_cast<float>(extent.height);
+
+    VkRect2D scissor = {};
+    scissor.offset = {0, 0};
+    scissor.extent = extent;
+
+    VkPipelineViewportStateCreateInfo viewportState = {};
+    viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewportState.viewportCount = 1;
+    viewportState.pViewports = &viewport;
+    viewportState.scissorCount = 1;
+    viewportState.pScissors = &scissor;
+
+    // set up rsterizer
+    VkPipelineRasterizationStateCreateInfo rasterizer = {};
+    rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    rasterizer.depthClampEnable = VK_FALSE;
+    rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+    rasterizer.lineWidth = 1.0f;
+    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizer.depthBiasEnable = VK_FALSE;
+
+    // set up multisampling (off)
+    VkPipelineMultisampleStateCreateInfo multisampling = {};
+    multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    multisampling.sampleShadingEnable =VK_FALSE;
+    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+    // color blending
+    VkPipelineColorBlendAttachmentState colorBlendAttachment = {};
+    colorBlendAttachment.blendEnable = VK_FALSE;
+    colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT |
+                                          VK_COLOR_COMPONENT_G_BIT |
+                                          VK_COLOR_COMPONENT_B_BIT |
+                                          VK_COLOR_COMPONENT_A_BIT;
+
+    VkPipelineColorBlendStateCreateInfo colorBlending = {};
+    colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    colorBlending.logicOpEnable = VK_FALSE;
+    colorBlending.logicOp = VK_LOGIC_OP_COPY;
+    colorBlending.attachmentCount = 1;
+    colorBlending.pAttachments = &colorBlendAttachment;
+    colorBlending.blendConstants[0] = 0.0f;
+    colorBlending.blendConstants[1] = 0.0f;
+    colorBlending.blendConstants[2] = 0.0f;
+    colorBlending.blendConstants[3] = 0.0f;
+
+    VkGraphicsPipelineCreateInfo pipelineInfo = {};
+    pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipelineInfo.stageCount = 2;
+    pipelineInfo.pStages = shaderStages;
+    pipelineInfo.pVertexInputState = &vertexInputInfo;
+    pipelineInfo.pInputAssemblyState = &inputAssembly;
+    pipelineInfo.pViewportState = &viewportState;
+    pipelineInfo.pRasterizationState = &rasterizer;
+    pipelineInfo.pMultisampleState = &multisampling;
+    pipelineInfo.pColorBlendState = &colorBlending;
+    pipelineInfo.layout = *pipelineLayout;
+    pipelineInfo.renderPass = *renderPass;
+    pipelineInfo.subpass = 0;
+    pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+    VkPipeline pipeline;
+    if (vkCreateGraphicsPipelines(*device, VK_NULL_HANDLE, 1, &pipelineInfo, pAllocator, &pipeline ) == VK_SUCCESS)
+    {
+        return new Pipeline(device, pipeline, pAllocator);
+    }
+
+    return nullptr;
+}
 
 }
 
@@ -261,8 +414,18 @@ int main(int argc, char** argv)
     std::cout<<"Created swapchain"<<std::endl;
 
     vsg::ref_ptr<vsg::ShaderModule> vert = vsg::readShaderModule(device.get(), "shaders/vert.spv");
-    vsg::ref_ptr<vsg::ShaderModule> frag = vsg::readShaderModule(device.get(), "shaders/frag.spv");
+    if (!vert)
+    {
+        std::cout<<"Could not find vertex shader"<<std::endl;
+        return 1;
+    }
 
+    vsg::ref_ptr<vsg::ShaderModule> frag = vsg::readShaderModule(device.get(), "shaders/frag.spv");
+    if (!frag)
+    {
+        std::cout<<"Could not find fragment shader"<<std::endl;
+        return 1;
+    }
 
     vsg::ref_ptr<vsg::Viewport> viewport = new vsg::Viewport;
     (*viewport).width = 10.0f;
@@ -270,6 +433,13 @@ int main(int argc, char** argv)
     vsg::ref_ptr<vsg::RenderPass> renderPass = new vsg::RenderPass(device.get(), swapchain->getImageFormat());
     std::cout<<"Created RenderPass"<<std::endl;
 
+    vsg::ref_ptr<vsg::PipelineLayout> pipelineLayout = new vsg::PipelineLayout(device.get());
+
+    std::cout<<"Created PipelineLayout "<<pipelineLayout.get()<<std::endl;
+
+    vsg::ref_ptr<vsg::Pipeline> pipeline = vsg::createGraphicsPipeline(device.get(), swapchain.get(), renderPass.get(), pipelineLayout.get(), vert.get(), frag.get());
+
+    std::cout<<"Created GraphicsPipline "<<pipeline.get()<<std::endl;
 
     //
     // end of initialize vulkan
