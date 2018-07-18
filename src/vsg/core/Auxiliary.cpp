7e0c39f199c0295fb84d7978a4add63895733394
Robert Osfield <robert@openscenegraph.com>
2018-07-18 18:06:41 +0100

    Quietened down debug output

--- src/vsg/core/Auxiliary.cpp
+++ src/vsg/core/Auxiliary.cpp
@@ -8,23 +8,23 @@ Auxiliary::Auxiliary() :
     _referenceCount(0),
     _connectedObject(0)
 {
-    std::cout<<"Auxiliary::Auxiliary() "<<this<<" "<<_objectMap.size()<<std::endl;
+    //std::cout<<"Auxiliary::Auxiliary() "<<this<<" "<<_objectMap.size()<<std::endl;
 }
 
 Auxiliary::~Auxiliary()
 {
-    std::cout<<"Auxiliary::~Auxiliary() "<<this<<std::endl;
+    //std::cout<<"Auxiliary::~Auxiliary() "<<this<<std::endl;
 }
 
 void Auxiliary::ref() const
 {
     ++_referenceCount;
-    std::cout<<"Auxiliary::ref() "<<this<<" "<<_referenceCount.load()<<std::endl;
+    //std::cout<<"Auxiliary::ref() "<<this<<" "<<_referenceCount.load()<<std::endl;
 }
 
 void Auxiliary::unref() const
 {
-    std::cout<<"Auxiliary::unref() "<<this<<" "<<_referenceCount.load()<<std::endl;
+    //std::cout<<"Auxiliary::unref() "<<this<<" "<<_referenceCount.load()<<std::endl;
     if (_referenceCount.fetch_sub(1)<=1)
     {
         delete this;
@@ -33,7 +33,7 @@ void Auxiliary::unref() const
 
 void Auxiliary::unref_nodelete() const
 {
-    std::cout<<"Auxiliary::unref_nodelete() "<<this<<" "<<_referenceCount.load()<<std::endl;
+    //std::cout<<"Auxiliary::unref_nodelete() "<<this<<" "<<_referenceCount.load()<<std::endl;
     --_referenceCount;
 }
 
@@ -41,18 +41,18 @@ void Auxiliary::unref_nodelete() const
 void Auxiliary::setConnectedObject(Object* object)
 {
     _connectedObject = object;
-    std::cout<<"Auxiliary::setConnectedObject("<<object<<") previous _connectedObject="<<_connectedObject<<std::endl;
+    //std::cout<<"Auxiliary::setConnectedObject("<<object<<") previous _connectedObject="<<_connectedObject<<std::endl;
 }
 
 void Auxiliary::setObject(const Object::Key& key, Object* object)
 {
     _objectMap[key] = object;
-    std::cout<<"Auxiliary::setObject( ["<<key.name<<", "<<key.index<<"], "<<object<<")"<<" "<<_objectMap.size()<<" "<<&_objectMap<<std::endl;
+    //std::cout<<"Auxiliary::setObject( ["<<key.name<<", "<<key.index<<"], "<<object<<")"<<" "<<_objectMap.size()<<" "<<&_objectMap<<std::endl;
 }
 
 Object* Auxiliary::getObject(const Object::Key& key)
 {
-    std::cout<<"Auxiliary::getObject( ["<<key.name<<", "<<key.index<<"])"<<std::endl;
+    //std::cout<<"Auxiliary::getObject( ["<<key.name<<", "<<key.index<<"])"<<std::endl;
     ObjectMap::iterator itr = _objectMap.find(key);
     if (itr != _objectMap.end()) return itr->second.get();
     else return nullptr;
@@ -60,7 +60,7 @@ Object* Auxiliary::getObject(const Object::Key& key)
 
 const Object* Auxiliary::getObject(const Object::Key& key) const
 {
-    std::cout<<"Auxiliary::getObject( ["<<key.name<<", "<<key.index<<"]) const"<<std::endl;
+    //std::cout<<"Auxiliary::getObject( ["<<key.name<<", "<<key.index<<"]) const"<<std::endl;
     ObjectMap::const_iterator itr = _objectMap.find(key);
     if (itr != _objectMap.end()) return itr->second.get();
     else return nullptr;
