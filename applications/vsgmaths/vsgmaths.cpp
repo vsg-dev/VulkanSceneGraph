55eea9e763a001df97cb50248e2c793a7e7704bf
Robert Osfield <robert@openscenegraph.com>
2018-07-19 17:29:42 +0100

    Moved rotate, translate and scale functions into maths/transform.h header

--- applications/vsgmaths/vsgmaths.cpp
+++ applications/vsgmaths/vsgmaths.cpp
@@ -8,6 +8,7 @@
 #include <vsg/maths/vec3.h>
 #include <vsg/maths/vec4.h>
 #include <vsg/maths/mat4.h>
+#include <vsg/maths/transform.h>
 
 #include <iostream>
 #include <vector>
@@ -56,68 +57,6 @@ inline std::ostream& operator << (std::ostream& output, const vsg::tmat4<T>& mat
 
 namespace vsg
 {
-
-    template<typename T>
-    tmat4<T> perspective(T fovy_radians, T aspectRatio, T zNear, T zFar)
-    {
-        T f = 1.0/tan(fovy_radians*0.5);
-        T r = 1.0/(zNear-zFar);
-        return tmat4<T>(f/aspectRatio, 0,  0,              0,
-                        0,             -f, 0,              0,
-                        0,             0,  (zFar+zNear)*r, (2.0*zFar*zNear)*r,
-                        0,             0,  -1,             0);
-    }
-
-    template<typename T>
-    tmat4<T> lookAt(tvec3<T> const & eye, tvec3<T> const & center, tvec3<T> const & up )
-    {
-        using vec_type = tvec3<T>;
-
-        vec_type forward = normalize(center-eye);
-        vec_type up_normal = normalize(up);
-        vec_type side = normalize(cross(forward, up_normal));
-        vec_type u = normalize(cross(side, forward));
-
-        return tmat4<T>(side[0],     side[1],     side[2],      0,
-                        u[0],        u[1],        u[2],         0,
-                        -forward[0], -forward[1], -forward[2],  0,
-                        0,           0,           0,            1);
-    }
-
-    template<typename T>
-    tmat4<T> rotate(T angle_radians, T x, T y, T z)
-    {
-        const T c = cos(angle_radians);
-        const T s = sin(angle_radians);
-        const T one_minus_c = 1-c;
-        return tmat4<T>(x*x*one_minus_c+c,     x*y*one_minus_c-z*s, x*z*one_minus_c+y*z, 0,
-                        y*x*one_minus_c+z*s,   y*y*one_minus_c+c,   y*z*one_minus_c-x*s, 0,
-                        x*z*one_minus_c-y*s,   y*z*one_minus_c+x*s, z*z*one_minus_c+c,   0,
-                        0,                     0,                   0,                   1);
-    }
-
-    template<typename T>
-    tmat4<T> rotate(T angle_radians, const tvec3<T>& v)
-    {
-        return (angle_radians, v.x, v.y, v.z);
-    }
-
-    template<typename T>
-    tmat4<T> translate(T x, T y, T z)
-    {
-        return tmat4<T>(1, 0, 0, x,
-                        0, 1, 0, y,
-                        0, 0, 1, z,
-                        0, 0, 0, 1);
-    }
-
-    template<typename T>
-    tmat4<T> translate(const tvec3<T>& v)
-    {
-        return translate(v.x, v.y, v.z);
-    }
-
-
     const float PIf   = 3.14159265358979323846f;
     const double PI   = 3.14159265358979323846;
 
@@ -239,6 +178,14 @@ int main(int argc, char** argv)
     std::cout<<"delta for trans "<<computeDelta(trans, osg_trans)<<std::endl;
     std::cout<<"trans = {"<<trans<<"}"<<std::endl;
     std::cout<<"osg_trans = {"<<osg_trans<<"}"<<std::endl;
+
+
+    vsg::mat4 scale = vsg::scale(vsg::vec3(1.0f, 2.0f, 3.0f));
+    osg::Matrixf osg_scale = osg::Matrixf::scale(1.0f, 2.0f, 3.0f);
+
+    std::cout<<"delta for scale "<<computeDelta(scale, osg_scale)<<std::endl;
+    std::cout<<"scale = {"<<scale<<"}"<<std::endl;
+    std::cout<<"osg_scale = {"<<osg_scale<<"}"<<std::endl;
 
     return 0;
 }
