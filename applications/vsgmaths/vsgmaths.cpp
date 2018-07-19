767a4f8fb6c5872d08b378773272c8a5f866f42e
Robert Osfield <robert@openscenegraph.com>
2018-07-19 15:35:25 +0100

    Added vsg::perspective() and vsg::lookAt() implementations and tests to compare results to OSG equivilants.  Note, vsg::perspective() accounts for Vulkan having the y axis of window coords at top, rather than bottom that OpenGL uses

--- applications/vsgmaths/vsgmaths.cpp
+++ applications/vsgmaths/vsgmaths.cpp
@@ -15,6 +15,9 @@
 #include <cstddef>
 
 
+#include <osg/Matrixd>
+#include <osg/io_utils>
+
 ////////////////////////////////////////////////////////////////////
 //
 //  ostream implementation
@@ -41,13 +44,80 @@ inline std::ostream& operator << (std::ostream& output, const vsg::tvec4<T>& vec
 }
 
 template<typename T>
-inline std::ostream& operator << (std::ostream& output, const vsg::tmat4<T>& vec)
+inline std::ostream& operator << (std::ostream& output, const vsg::tmat4<T>& mat)
 {
-    output << vec(0,0)<< " " << vec(0,1)<<" "<<vec(0,2)<<" "<<vec(0,3)<<std::endl;
-    output << vec(1,0)<< " " << vec(1,1)<<" "<<vec(1,2)<<" "<<vec(1,3)<<std::endl;
-    output << vec(2,0)<< " " << vec(2,1)<<" "<<vec(2,2)<<" "<<vec(2,3)<<std::endl;
-    output << vec(3,0)<< " " << vec(3,1)<<" "<<vec(3,2)<<" "<<vec(3,3)<<std::endl;
+    output << mat(0,0)<< " " << mat(1,0)<<" "<<mat(2,0)<<" "<<mat(3,0)<<std::endl;
+    output << mat(0,1)<< " " << mat(1,1)<<" "<<mat(2,1)<<" "<<mat(3,1)<<std::endl;
+    output << mat(0,2)<< " " << mat(1,2)<<" "<<mat(2,2)<<" "<<mat(3,2)<<std::endl;
+    output << mat(0,3)<< " " << mat(1,3)<<" "<<mat(2,3)<<" "<<mat(3,3)<<std::endl;
     return output; // to enable cascading
+}
+
+namespace vsg
+{
+
+    template<typename T>
+    tmat4<T> perspective(T fovy_radians, T aspectRatio, T zNear, T zFar)
+    {
+        T f = 1.0/tan(fovy_radians*0.5);
+        T r = 1.0/(zNear-zFar);
+        return tmat4<T>(f/aspectRatio, 0,  0,              0,
+                        0,             -f, 0,              0,
+                        0,             0,  (zFar+zNear)*r, (2.0*zFar*zNear)*r,
+                        0,             0,  -1,             0);
+    }
+
+    template<typename T>
+    tmat4<T> lookAt(tvec3<T> const & eye, tvec3<T> const & center, tvec3<T> const & up )
+    {
+        using vec_type = tvec3<T>;
+        using mat_type = tmat4<T>;
+
+        vec_type forward = normalize(center-eye);
+        vec_type up_normal = normalize(up);
+        vec_type side = normalize(cross(forward, up_normal));
+        vec_type u = normalize(cross(side, forward));
+
+        return mat_type(side[0],     side[1],     side[2],      0,
+                        u[0],        u[1],        u[2],         0,
+                        -forward[0], -forward[1], -forward[2],  0,
+                        0,           0,           0,            1);
+    }
+
+    const float PIf   = 3.14159265358979323846f;
+    const double PI   = 3.14159265358979323846;
+
+    float radians(float degrees) { return degrees * (PIf/180.0f); }
+    double radians(double degrees) { return degrees * (PI/180.0); }
+
+    float degrees(float radians) { return radians * (180.0f/PIf); }
+    double degrees(double radians) { return radians * (180.0/PI); }
+};
+
+float computeDelta(const vsg::mat4& v, const osg::Matrixf& o)
+{
+    vsg::mat4::value_type delta = 0.0f;
+
+    const vsg::mat4::value_type* view_ptr = v.data();
+    const osg::Matrixf::value_type* osg_view_ptr = o.ptr();
+    for(int i=0; i<16; ++i)
+    {
+        delta += abs(view_ptr[i]-osg_view_ptr[i]);
+    }
+    return delta;
+}
+
+float computeDelta(const vsg::dmat4& v, const osg::Matrixd& o)
+{
+    vsg::dmat4::value_type delta = 0.0f;
+
+    const vsg::dmat4::value_type* view_ptr = v.data();
+    const osg::Matrixd::value_type* osg_view_ptr = o.ptr();
+    for(int i=0; i<16; ++i)
+    {
+        delta += abs(view_ptr[i]-osg_view_ptr[i]);
+    }
+    return delta;
 }
 
 int main(int argc, char** argv)
@@ -62,7 +132,6 @@ int main(int argc, char** argv)
     std::cout<<"vec2(x="<<v.x<<", y="<<v.y<<")"<<std::endl;
     std::cout<<"vec2(r="<<v.r<<", g="<<v.g<<")"<<std::endl;
     std::cout<<"vec2(s="<<v.s<<", t="<<v.t<<")"<<std::endl;
-    std::cout<<"vec2.data="<<v.data<<" ("<<v.data[0]<<", "<<v.data[1]<<")"<<std::endl;
     std::cout<<"vec2[0]=("<<v[0]<<", "<<v[1]<<")"<<std::endl;
 
     vsg::dvec3 n(2.0, 1.0, 0.5);
@@ -93,6 +162,34 @@ int main(int argc, char** argv)
     {
         std::cout<<"getValue(\"matrix\""<<new_mat<<std::endl;
     }
+
+
+    vsg::mat4 proj = vsg::perspective(vsg::radians(45.0), 2.0, 0.1, 10.0);
+
+    std::cout<<"proj = {"<<std::endl<<proj<<"}"<<std::endl;
+
+    osg::Matrixf osg_proj = osg::Matrixf::perspective(45.0f, 2.0f, 0.1f, 10.0f);
+    std::cout<<"osg_proj = "<<osg_proj<<std::endl;
+
+    std::cout<<std::endl;
+
+    vsg::mat4 view = vsg::lookAt(vsg::vec3(0,0,0), vsg::vec3(0,10,0), vsg::vec3(0,0,1));
+
+    std::cout<<"view = {"<<std::endl<<view<<"}"<<std::endl;
+
+    osg::Matrixf osg_view = osg::Matrixf::lookAt(osg::Vec3(0,0,0), osg::Vec3(0,10, 0), osg::Vec3(0,0,1));
+    std::cout<<"osg_view = "<<osg_view<<std::endl;
+
+
+    float* view_ptr = view.data();
+    float* osg_view_ptr = osg_view.ptr();
+    for(int i=0; i<16; ++i)
+    {
+        std::cout<<" view_ptr["<<i<<"]="<<view_ptr[i]<<"   osg_voew_ptr["<<i<<"]="<<osg_view_ptr[i]<<std::endl;
+    }
+
+    std::cout<<"delta for proj "<<computeDelta(proj, osg_proj)<<std::endl;
+    std::cout<<"delta for view "<<computeDelta(view, osg_view)<<std::endl;
 
     return 0;
 }
