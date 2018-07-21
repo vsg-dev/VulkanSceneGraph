f7d45d0e7ceb2e139d74a00501b8b5cdd195ca8e
Robert Osfield <robert@openscenegraph.com>
2018-07-21 11:55:40 +0100

    Added vsg::DescriptorPool wrapped of vkDescriptorPool

---
+++ include/vsg/vk/DescriptorPool.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vsg/vk/Device.h>
+
+namespace vsg
+{
+
+    using DescriptorPoolSizes = std::vector<VkDescriptorPoolSize>;
+
+    class DescriptorPool : public vsg::Object
+    {
+    public:
+        DescriptorPool(Device* device, VkDescriptorPool descriptorPool, AllocationCallbacks* allocator=nullptr);
+
+        using Result = vsg::Result<DescriptorPool, VkResult, VK_SUCCESS>;
+
+        static Result create(Device* device, uint32_t maxSets, const DescriptorPoolSizes& descriptorPoolSizes, AllocationCallbacks* allocator=nullptr);
+
+        operator const VkDescriptorPool& () const { return _descriptorPool; }
+
+    protected:
+        virtual ~DescriptorPool();
+
+        ref_ptr<Device>                 _device;
+        VkDescriptorPool                _descriptorPool;
+        ref_ptr<AllocationCallbacks>    _allocator;
+    };
+
+}
---
+++ src/vsg/vk/DescriptorPool.cpp
@@ -0,0 +1,60 @@
+#include <vsg/vk/DescriptorPool.h>
+
+#include <iostream>
+
+namespace vsg
+{
+
+DescriptorPool::DescriptorPool(Device* device, VkDescriptorPool descriptorPool, AllocationCallbacks* allocator) :
+    _device(device),
+    _descriptorPool(descriptorPool),
+    _allocator(allocator)
+{
+}
+
+DescriptorPool::~DescriptorPool()
+{
+    if (_descriptorPool)
+    {
+        std::cout<<"Calling vkDestroyDescriptorPool"<<std::endl;
+        vkDestroyDescriptorPool(*_device, _descriptorPool, *_allocator);
+    }
+}
+
+DescriptorPool::Result DescriptorPool::create(Device* device, uint32_t maxSets, const DescriptorPoolSizes& descriptorPoolSizes, AllocationCallbacks* allocator)
+{
+    if (!device)
+    {
+        return Result("Error: vsg::DescriptorPool::create(...) failed to create DescriptorPool, undefined Device.", VK_ERROR_INVALID_EXTERNAL_HANDLE);
+    }
+
+    if (!device)
+    {
+        return Result("Error: vsg::DescriptorPool::create(...) failed to create DescriptorPool, descriptorPoolSizes.empty().", VK_ERROR_INITIALIZATION_FAILED);
+    }
+
+        std::cout<<"DescriptorPool::create(device, "<<maxSets<<std::endl;
+    for(auto poolSize : descriptorPoolSizes)
+    {
+        std::cout<<"   "<<poolSize.type<<" "<<poolSize.descriptorCount<<std::endl;
+    }
+
+    VkDescriptorPoolCreateInfo poolInfo = {};
+    poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    poolInfo.poolSizeCount = descriptorPoolSizes.size();
+    poolInfo.pPoolSizes = descriptorPoolSizes.data();
+    poolInfo.maxSets = maxSets;
+
+    VkDescriptorPool descriptorPool;
+    VkResult result = vkCreateDescriptorPool(*device, &poolInfo, *allocator, &descriptorPool);
+    if (result == VK_SUCCESS)
+    {
+        return new DescriptorPool(device, descriptorPool, allocator);
+    }
+    else
+    {
+        return Result("Error: Failed to create DescriptorPool.", result);
+    }
+}
+
+}
