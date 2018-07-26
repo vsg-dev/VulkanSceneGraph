8db79ea26a82729378390dfbe4108dc61f7a6bab
Robert Osfield <robert@openscenegraph.com>
2018-07-26 18:18:12 +0100

    Fixed anisotropy setup

--- src/vsg/vk/Device.cpp
+++ src/vsg/vk/Device.cpp
@@ -44,6 +44,7 @@ Device::Result Device::create(Instance* instance, PhysicalDevice* physicalDevice
     }
 
     VkPhysicalDeviceFeatures deviceFeatures = {};
+    deviceFeatures.samplerAnisotropy = VK_TRUE;
 
     VkDeviceCreateInfo createInfo = {};
     createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
--- src/vsg/vk/Sampler.cpp
+++ src/vsg/vk/Sampler.cpp
@@ -49,8 +49,8 @@ Sampler::Result Sampler::create(Device* device, AllocationCallbacks* allocator)
     samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
     samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
     samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-#if 0
-    // currently failing even though the physcialDevice reports supportedFeatures.samplerAnisotrop is 1
+#if 1
+    // requres Logical device to have deviceFeatures.samplerAnisotropy = VK_TRUE; set when creating the vsg::Deivce
     samplerInfo.anisotropyEnable = VK_TRUE;
     samplerInfo.maxAnisotropy = 16;
 #else
