499d5cb1e493a8a14b97f1e36dae4fa096bf6db4
Robert Osfield <robert@openscenegraph.com>
2018-07-31 20:28:14 +0100

    Added vsg::CommandVisitor to vsg lib

---
+++ include/vsg/vk/CommandVisitor.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vsg/core/Visitor.h>
+
+#include <vsg/vk/Framebuffer.h>
+#include <vsg/vk/CommandBuffer.h>
+#include <vsg/vk/RenderPass.h>
+#include <vsg/vk/Command.h>
+
+namespace vsg
+{
+
+    class CommandVisitor : public Visitor
+    {
+    public:
+
+        ref_ptr<Framebuffer>    _framebuffer;
+        VkCommandBuffer         _commandBuffer;
+        VkExtent2D              _extent;
+        VkClearValue            _clearColor;
+
+        CommandVisitor(Framebuffer* framebuffer, VkCommandBuffer commandBuffer, const VkExtent2D& extent, const VkClearValue& clearColor);
+
+        void apply(Object& object);
+
+        void apply(Node& object);
+
+        void apply(Command& cmd);
+
+        void apply(RenderPass& renderPass);
+
+        void apply(CommandBuffer& commandBuffer);
+
+        void populateCommandBuffer(vsg::Node* subgraph);
+    };
+
+
+}
---
+++ src/vsg/vk/CommandVisitor.cpp
@@ -0,0 +1,107 @@
+#include <vsg/vk/CommandVisitor.h>
+
+#include <iostream>
+#include <typeinfo>
+
+namespace vsg
+{
+
+
+template<typename T>
+struct StoreAndRestore
+{
+    using P = T*;
+
+    T _value;
+    P _ptr;
+
+    StoreAndRestore(T& value) : _value(value), _ptr(&value) {}
+    ~StoreAndRestore() { *_ptr = _value; }
+};
+
+
+
+CommandVisitor::CommandVisitor(Framebuffer* framebuffer, VkCommandBuffer commandBuffer, const VkExtent2D& extent, const VkClearValue& clearColor) :
+    _framebuffer(framebuffer),
+    _commandBuffer(commandBuffer),
+    _extent(extent),
+    _clearColor(clearColor)
+{
+}
+
+void CommandVisitor::apply(Object& object)
+{
+    std::cout<<"Visiting internal object : "<<typeid(object).name()<<std::endl;
+    object.traverse(*this);
+}
+
+void CommandVisitor::apply(Node& object)
+{
+    std::cout<<"Visiting internal node : "<<typeid(object).name()<<std::endl;
+    object.traverse(*this);
+}
+
+void CommandVisitor::apply(Command& cmd)
+{
+    std::cout<<"Visiting leaf node : "<<typeid(cmd).name()<<std::endl;
+    cmd.dispatch(_commandBuffer);
+}
+
+void CommandVisitor::apply(RenderPass& renderPass)
+{
+    std::cout<<"Visiting RenderPass : "<<typeid(renderPass).name()<<std::endl;
+
+    VkRenderPassBeginInfo renderPassInfo = {};
+    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+    renderPassInfo.renderPass = renderPass;
+    renderPassInfo.framebuffer = *_framebuffer;
+    renderPassInfo.renderArea.offset = {0, 0};
+    renderPassInfo.renderArea.extent = _extent;
+
+    renderPassInfo.clearValueCount = 1;
+    renderPassInfo.pClearValues = &_clearColor;
+    vkCmdBeginRenderPass(_commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+        renderPass.traverse(*this);
+
+    vkCmdEndRenderPass(_commandBuffer);
+}
+
+void CommandVisitor::apply(CommandBuffer& commandBuffer)
+{
+    std::cout<<"Visiting CommandBuffer : "<<typeid(commandBuffer).name()<<std::endl;
+
+    StoreAndRestore<VkCommandBuffer> temp(_commandBuffer);
+
+    // make this CommandBuffer the current one to use for all operations
+    _commandBuffer = commandBuffer;
+
+    VkCommandBufferBeginInfo beginInfo = {};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    beginInfo.flags = commandBuffer.flags();
+    // if we are nested within a CommandBuffer already then use VkCommandBufferInheritanceInfo
+
+    vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+        commandBuffer.traverse(*this);
+
+    vkEndCommandBuffer(commandBuffer);
+
+    std::cout<<"End visit CommandBuffer"<<std::endl;
+}
+
+void CommandVisitor::populateCommandBuffer(vsg::Node* subgraph)
+{
+    VkCommandBufferBeginInfo beginInfo = {};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
+    // if we are nested within a CommandBuffer already then use VkCommandBufferInheritanceInfo
+
+    vkBeginCommandBuffer(_commandBuffer, &beginInfo);
+
+        subgraph->accept(*this);
+
+    vkEndCommandBuffer(_commandBuffer);
+}
+
+}
