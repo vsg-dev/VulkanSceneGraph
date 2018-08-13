09e677a61b06dcca3348126a1b9a9705a70ddd3b
Robert Osfield <robert@openscenegraph.com>
2018-08-13 14:59:30 +0100

    Added background info on Vulkan memory management

--- src/vsg/vk/MemoryManager.cpp
+++ src/vsg/vk/MemoryManager.cpp
@@ -5,6 +5,28 @@
 #include <iostream>
 #include <cstring>
 
+// Useful links:
+//    AMD's Vulan Device Memory discussion : https://gpuopen.com/vulkan-device-memory/
+//    NVIDIA's Vulkan Memory Management  : https://developer.nvidia.com/vulkan-memory-management
+//
+// General comments
+//
+// Deivce local resources - use VkMemoryPropertyFlags of VK_DEVICE_LOCAL and not HOST_VISIBLE
+//
+// If Divice local allocations fail fallbac to HOST_VISIBLE with HOST_COHERENT but without HOST_CACHED
+//
+// Allocate high prioroty resources first i.e. Render Targets, then lower priotity objects
+//
+// On Window resizes be prepared to free all resources, then reallocated high priority ones
+//
+// CPU->GPU data flow Use DEVICE_LOCAL with HOST_VISIBLE for cases where you want to update Device local memory, ie. updloading constant data, keep allocation sizes below 256MB)
+//
+// GPU->CPU data flow use HOST_VISIBLE with HOST_COHERENT and HOST_CACHED, only memory type that supports cached reads by the CPU.  Screen captures, compute results etc.
+//
+// Pool resources as OS Window allocations can be expensive, place buffers and textures in single pools, 256MB is reasonalbe base
+//
+
+
 namespace vsg
 {
 
