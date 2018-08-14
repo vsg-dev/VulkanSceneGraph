c7c539b5ff665b4e0a09579e82617489bd0cf03a
Robert Osfield <robert@openscenegraph.com>
2018-08-14 15:01:27 +0100

    Added vsgmemory application for future testing of memory management code

---
+++ applications/vsgmemory/vsgmemory.cpp
@@ -0,0 +1,28 @@
+#include <vsg/core/ref_ptr.h>
+#include <vsg/core/observer_ptr.h>
+#include <vsg/core/Object.h>
+#include <vsg/core/Auxiliary.h>
+
+#include <vsg/nodes/Group.h>
+#include <vsg/nodes/QuadGroup.h>
+#include <vsg/nodes/LOD.h>
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <memory>
+#include <unordered_map>
+#include <stack>
+#include <typeindex>
+
+
+namespace vsg
+{
+}
+
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
