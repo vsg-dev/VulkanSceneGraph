1cb3d65131be8a5777e07e855a3cde6a27495371
Robert Osfield <robert@openscenegraph.com>
2018-10-11 11:18:56 +0100

    Added buill_all_h make target to automatically generate a new include/vsg/all.h file based on the current include/vsg/*/*.h files

---
+++ CMakeModules/build_all_h.cmake
@@ -0,0 +1,37 @@
+
+macro(APPEND_INCLUDES target_var source_var prefix)
+    set(${target_var} ${${target_var}} "\n" ${prefix})
+
+    foreach(value ${${source_var}})
+        set(${target_var} ${${target_var}} "#include <" ${value} ">\n")
+    endforeach()
+endmacro()
+
+macro(BUILD_ALL_H)
+
+    set(INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/include)
+
+    file(GLOB CORE_HEADERS RELATIVE ${INCLUDE_DIR} ${INCLUDE_DIR}/vsg/core/*.h )
+    file(GLOB INTROSPECTION_HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/include include/vsg/introspection/*.h )
+    file(GLOB MATHS_HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/include include/vsg/maths/*.h )
+    file(GLOB NODES_HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/include include/vsg/nodes/*.h )
+    file(GLOB TRAVERSAL_HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/include include/vsg/traversals/*.h )
+    file(GLOB UTILS_HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/include include/vsg/utils/*.h )
+    file(GLOB VIEWER_HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/include include/vsg/viewer/*.h )
+    file(GLOB VK_HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/include include/vsg/vk/*.h )
+
+    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/docs/header_license_preamble.txt ALL_H_CONTENTS)
+    APPEND_INCLUDES(ALL_H_CONTENTS CORE_HEADERS "// Core header files\n")
+    APPEND_INCLUDES(ALL_H_CONTENTS MATHS_HEADERS "// Maths header files\n")
+    APPEND_INCLUDES(ALL_H_CONTENTS NODES_HEADERS "// Node header files\n")
+    APPEND_INCLUDES(ALL_H_CONTENTS TRAVERSAL_HEADERS "// Traversal header files\n")
+    APPEND_INCLUDES(ALL_H_CONTENTS VIEWER_HEADERS "// Viewer header files\n")
+    APPEND_INCLUDES(ALL_H_CONTENTS VK_HEADERS "// Vulkan related header files\n")
+    APPEND_INCLUDES(ALL_H_CONTENTS UTILS_HEADERS "// Utiltiy header files\n")
+    APPEND_INCLUDES(ALL_H_CONTENTS INTROSPECTION_HEADERS "// Introspection header files\n")
+
+    file(WRITE include/vsg/all.h ${ALL_H_CONTENTS})
+
+endmacro()
+
+BUILD_ALL_H()
--- include/vsg/all.h
+++ include/vsg/all.h
@@ -12,9 +12,85 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 </editor-fold> */
 
-#include <vsg/core.h>
-#include <vsg/maths.h>
-#include <vsg/nodes.h>
-#include <vsg/utils.h>
-#include <vsg/viewer.h>
-#include <vsg/vk.h>
+// Core header files
+#include <vsg/core/Allocator.h>
+#include <vsg/core/Array.h>
+#include <vsg/core/Auxiliary.h>
+#include <vsg/core/Data.h>
+#include <vsg/core/Export.h>
+#include <vsg/core/Inherit.h>
+#include <vsg/core/Object.h>
+#include <vsg/core/Result.h>
+#include <vsg/core/Value.h>
+#include <vsg/core/Visitor.h>
+#include <vsg/core/observer_ptr.h>
+#include <vsg/core/ref_ptr.h>
+
+// Maths header files
+#include <vsg/maths/mat4.h>
+#include <vsg/maths/transform.h>
+#include <vsg/maths/vec2.h>
+#include <vsg/maths/vec3.h>
+#include <vsg/maths/vec4.h>
+
+// Node header files
+#include <vsg/nodes/FixedGroup.h>
+#include <vsg/nodes/Group.h>
+#include <vsg/nodes/LOD.h>
+#include <vsg/nodes/Node.h>
+#include <vsg/nodes/QuadGroup.h>
+#include <vsg/nodes/StateGroup.h>
+
+// Traversal header files
+#include <vsg/traversals/DispatchTraversal.h>
+
+// Viewer header files
+#include <vsg/viewer/GraphicsStage.h>
+#include <vsg/viewer/Viewer.h>
+#include <vsg/viewer/Window.h>
+
+// Vulkan related header files
+#include <vsg/vk/AllocationCallbacks.h>
+#include <vsg/vk/BindIndexBuffer.h>
+#include <vsg/vk/BindVertexBuffers.h>
+#include <vsg/vk/Buffer.h>
+#include <vsg/vk/BufferData.h>
+#include <vsg/vk/BufferView.h>
+#include <vsg/vk/Command.h>
+#include <vsg/vk/CommandBuffer.h>
+#include <vsg/vk/CommandPool.h>
+#include <vsg/vk/CommandVisitor.h>
+#include <vsg/vk/ComputePipeline.h>
+#include <vsg/vk/Descriptor.h>
+#include <vsg/vk/DescriptorPool.h>
+#include <vsg/vk/DescriptorSet.h>
+#include <vsg/vk/DescriptorSetLayout.h>
+#include <vsg/vk/Device.h>
+#include <vsg/vk/DeviceMemory.h>
+#include <vsg/vk/Draw.h>
+#include <vsg/vk/Fence.h>
+#include <vsg/vk/Framebuffer.h>
+#include <vsg/vk/GraphicsPipeline.h>
+#include <vsg/vk/Image.h>
+#include <vsg/vk/ImageView.h>
+#include <vsg/vk/Instance.h>
+#include <vsg/vk/MemoryManager.h>
+#include <vsg/vk/PhysicalDevice.h>
+#include <vsg/vk/Pipeline.h>
+#include <vsg/vk/PipelineLayout.h>
+#include <vsg/vk/PushConstants.h>
+#include <vsg/vk/RenderPass.h>
+#include <vsg/vk/Sampler.h>
+#include <vsg/vk/Semaphore.h>
+#include <vsg/vk/ShaderModule.h>
+#include <vsg/vk/State.h>
+#include <vsg/vk/Surface.h>
+#include <vsg/vk/Swapchain.h>
+
+// Utiltiy header files
+#include <vsg/utils/CommandLine.h>
+#include <vsg/utils/FileSystem.h>
+#include <vsg/utils/stream.h>
+
+// Introspection header files
+#include <vsg/introspection/c_interface.h>
