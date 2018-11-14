4a222039dc2c4e1be9c6667aeac58571e86b319e
Robert Osfield <robert@openscenegraph.com>
2018-11-14 10:52:27 +0000

    Removed vsg::FixedGroup as making a general purpose vsg::Group more efficient will be the objective instead

--- include/vsg/all.h
+++ include/vsg/all.h
@@ -36,7 +36,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <vsg/maths/vec4.h>
 
 // Node header files
-#include <vsg/nodes/FixedGroup.h>
 #include <vsg/nodes/Group.h>
 #include <vsg/nodes/LOD.h>
 #include <vsg/nodes/Node.h>
