b8ea5170b90b3442e7b04855996efcc089494d8e
Robert Osfield <robert@openscenegraph.com>
2018-11-24 17:39:56 +0000

    Removed unneccessary setting of cmke module path. Added Xcb find to vsgConfig.cmake

--- src/vsg/vsgConfig.cmake
+++ src/vsg/vsgConfig.cmake
@@ -2,8 +2,17 @@ include(CMakeFindDependencyMacro)
 
 find_dependency(Vulkan)
 
-if(NOT WIN32)
+if (ANDROID)
+    # TODO
+elseif (WIN32)
+    # just use native windowing
+elseif (APPLE)
     find_dependency(glfw3)
+else()
+    find_dependency(glfw3)
+
+    find_package(PkgConfig)
+    pkg_check_modules(xcb REQUIRED IMPORTED_TARGET xcb)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/vsgTargets.cmake")
