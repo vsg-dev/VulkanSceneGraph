d2e4582b69d9b5cff370e0e97ae6d9a4610d7af4
Robert Osfield <robert@openscenegraph.com>
2018-12-03 15:08:22 +0000

    Added KeySymbol::KEY_0 to KEY_9 entries

--- include/vsg/ui/KeyEvent.h
+++ include/vsg/ui/KeyEvent.h
@@ -25,6 +25,17 @@ namespace vsg
 
         KEY_Space = 0x20,
 
+        KEY_0 = '0',
+        KEY_1 = '1',
+        KEY_2 = '2',
+        KEY_3 = '3',
+        KEY_4 = '4',
+        KEY_5 = '5',
+        KEY_6 = '6',
+        KEY_7 = '7',
+        KEY_8 = '8',
+        KEY_9 = '9',
+
         KEY_a = 'a',
         KEY_b = 'b',
         KEY_c = 'c',
