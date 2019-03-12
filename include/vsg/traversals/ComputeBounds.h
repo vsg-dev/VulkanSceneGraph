0d2e63373605e69b48e737cdef01a67bc08958a5
Robert Osfield <robert@openscenegraph.com>
2019-03-12 12:19:14 +0000

    Added ComputeBounds from osg2vsg

--- include/vsg/traversals/ComputeBounds.h
+++ include/vsg/traversals/ComputeBounds.h
@@ -12,43 +12,27 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 </editor-fold> */
 
-#include <vsg/maths/vec3.h>
-
-#include <limits>
+#include <vsg/core/ConstVisitor.h>
+#include <vsg/maths/box.h>
 
 namespace vsg
 {
-    template<typename T>
-    struct t_box
+
+    class VSG_DECLSPEC ComputeBounds : public vsg::ConstVisitor
     {
-        using value_type = T;
-        using vec_type = t_vec3<T>;
+    public:
+        ComputeBounds();
 
-        vec_type min = vec_type(std::numeric_limits<value_type>::max(), std::numeric_limits<value_type>::max(), std::numeric_limits<value_type>::max());
-        vec_type max = vec_type(std::numeric_limits<value_type>::lowest(), std::numeric_limits<value_type>::lowest(), std::numeric_limits<value_type>::lowest());
+        vsg::dbox bounds;
 
-        bool valid() const { return min.x <= max.x; }
+        using MatrixStack = std::vector<vsg::mat4>;
+        MatrixStack matrixStack;
 
-        template<typename R>
-        void add(const t_vec3<R>& v)
-        {
-            add(v.x, v.y, v.z);
-        }
-
-        void add(value_type x, value_type y, value_type z)
-        {
-            if (x < min.x) min.x = x;
-            if (y < min.y) min.y = y;
-            if (z < min.z) min.z = z;
-            if (x > max.x) max.x = x;
-            if (y > max.y) max.y = y;
-            if (z > max.z) max.z = z;
-        }
+        void apply(const vsg::Node& node);
+        void apply(const vsg::Group& group);
+        void apply(const vsg::MatrixTransform& transform);
+        void apply(const vsg::Geometry& geometry);
+        void apply(const vsg::vec3Array& vertices);
     };
 
-    using box = t_box<float>;
-    using dbox = t_box<double>;
-
-    VSG_type_name(vsg::box);
-    VSG_type_name(vsg::dbox);
-} // namespace vsg
+} // namespace vsg;
---
+++ src/vsg/traversals/ComputeBounds.cpp
@@ -0,0 +1,72 @@
+/* <editor-fold desc="MIT License">
+
+Copyright(c) 2018 Robert Osfield
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+</editor-fold> */
+
+#include <vsg/traversals/ComputeBounds.h>
+#include <vsg/nodes/MatrixTransform.h>
+#include <vsg/nodes/Geometry.h>
+
+using namespace vsg;
+
+ComputeBounds::ComputeBounds()
+{
+}
+
+void ComputeBounds::apply(const vsg::Node& node)
+{
+    node.traverse(*this);
+}
+
+void ComputeBounds::apply(const vsg::Group& group)
+{
+    if (auto transform = dynamic_cast<const vsg::MatrixTransform*>(&group); transform!=nullptr)
+    {
+        apply(*transform);
+    }
+    else if (auto geometry = dynamic_cast<const vsg::Geometry*>(&group); geometry!=nullptr)
+    {
+        apply(*geometry);
+    }
+    else
+    {
+        group.traverse(*this);
+    }
+}
+
+void ComputeBounds::apply(const vsg::MatrixTransform& transform)
+{
+    matrixStack.push_back(transform.getMatrix());
+
+    transform.traverse(*this);
+
+    matrixStack.pop_back();
+}
+
+void ComputeBounds::apply(const vsg::Geometry& geometry)
+{
+    if (!geometry._arrays.empty())
+    {
+        geometry._arrays[0]->accept(*this);
+    }
+}
+
+void ComputeBounds::apply(const vsg::vec3Array& vertices)
+{
+    if (matrixStack.empty())
+    {
+        for(auto vertex : vertices) bounds.add(vertex);
+    }
+    else
+    {
+        auto matrix = matrixStack.back();
+        for(auto vertex : vertices) bounds.add(matrix * vertex);
+    }
+}
