8417d7d88a8f878d2becc6013fd891698b6e810f
Robert Osfield <robert@openscenegraph.com>
2019-03-26 15:59:39 +0000

    Update all.h

--- include/vsg/all.h
+++ include/vsg/all.h
@@ -35,6 +35,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <vsg/maths/box.h>
 #include <vsg/maths/mat3.h>
 #include <vsg/maths/mat4.h>
+#include <vsg/maths/plane.h>
+#include <vsg/maths/quat.h>
 #include <vsg/maths/sphere.h>
 #include <vsg/maths/transform.h>
 #include <vsg/maths/vec2.h>
