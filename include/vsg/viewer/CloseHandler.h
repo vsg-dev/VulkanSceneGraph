4533eba030cf8f5f8bc5b9e294721c739bec4c14
sam <sbrkopac@users.noreply.github.com>
2019-08-18 16:31:31 -0600

    added missing include for CloseHandler

--- include/vsg/viewer/CloseHandler.h
+++ include/vsg/viewer/CloseHandler.h
@@ -13,6 +13,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 </editor-fold> */
 
 #include <vsg/viewer/Viewer.h>
+#include <vsg/ui/KeyEvent.h>
 
 namespace vsg
 {
