3d74b56ab7c85015653f04e72d58d2d5df8c2f58
Robert Osfield <robert@openscenegraph.com>
2019-09-13 15:03:26 +0100

    Added StaeteGroup::contains() and remove() methods

--- include/vsg/nodes/StateGroup.h
+++ include/vsg/nodes/StateGroup.h
@@ -19,6 +19,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #include <vsg/traversals/CompileTraversal.h>
 
+#include <algorithm>
+
 namespace vsg
 {
     // forward declare
@@ -37,9 +39,24 @@ namespace vsg
         StateCommands& getStateCommands() { return _stateCommands; }
         const StateCommands& getStateCommands() const { return _stateCommands; }
 
+        template<class T>
+        bool contains(const T value) const
+        {
+            return std::find(_stateCommands.begin(), _stateCommands.end(), value) != _stateCommands.end();
+        }
+
         void add(ref_ptr<StateCommand> stateCommand)
         {
             _stateCommands.push_back(stateCommand);
+        }
+
+        template<class T>
+        void remove(const T value)
+        {
+            if (auto itr = std::find(_stateCommands.begin(), _stateCommands.end(), value); itr != _stateCommands.end())
+            {
+                _stateCommands.erase(itr);
+            }
         }
 
         virtual void compile(Context& context);
