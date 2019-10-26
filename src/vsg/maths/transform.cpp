3b08f569e8efb1e7c741830c7a1a8bb742f08abf
Robert Osfield <robert@openscenegraph.com>
2019-10-26 09:36:34 +0100

    Added vsg::inverse(..) implementaton

---
+++ src/vsg/maths/transform.cpp
@@ -0,0 +1,77 @@
+/* <editor-fold desc="MIT License">
+
+Copyright(c) 2018 Robert Osfield
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+</editor-fold> */
+
+#include <vsg/maths/transform.h>
+
+using namespace vsg;
+
+template<class T>
+T t_inverse(const T& m)
+{
+    using value_type = typename T::value_type;
+
+    value_type A2323 = m[2][2] * m[3][3] - m[2][3] * m[3][2] ;
+    value_type A1323 = m[2][1] * m[3][3] - m[2][3] * m[3][1] ;
+    value_type A1223 = m[2][1] * m[3][2] - m[2][2] * m[3][1] ;
+    value_type A0323 = m[2][0] * m[3][3] - m[2][3] * m[3][0] ;
+    value_type A0223 = m[2][0] * m[3][2] - m[2][2] * m[3][0] ;
+    value_type A0123 = m[2][0] * m[3][1] - m[2][1] * m[3][0] ;
+    value_type A2313 = m[1][2] * m[3][3] - m[1][3] * m[3][2] ;
+    value_type A1313 = m[1][1] * m[3][3] - m[1][3] * m[3][1] ;
+    value_type A1213 = m[1][1] * m[3][2] - m[1][2] * m[3][1] ;
+    value_type A2312 = m[1][2] * m[2][3] - m[1][3] * m[2][2] ;
+    value_type A1312 = m[1][1] * m[2][3] - m[1][3] * m[2][1] ;
+    value_type A1212 = m[1][1] * m[2][2] - m[1][2] * m[2][1] ;
+    value_type A0313 = m[1][0] * m[3][3] - m[1][3] * m[3][0] ;
+    value_type A0213 = m[1][0] * m[3][2] - m[1][2] * m[3][0] ;
+    value_type A0312 = m[1][0] * m[2][3] - m[1][3] * m[2][0] ;
+    value_type A0212 = m[1][0] * m[2][2] - m[1][2] * m[2][0] ;
+    value_type A0113 = m[1][0] * m[3][1] - m[1][1] * m[3][0] ;
+    value_type A0112 = m[1][0] * m[2][1] - m[1][1] * m[2][0] ;
+
+    value_type det = m[0][0] * ( m[1][1] * A2323 - m[1][2] * A1323 + m[1][3] * A1223 )
+                   - m[0][1] * ( m[1][0] * A2323 - m[1][2] * A0323 + m[1][3] * A0223 )
+                   + m[0][2] * ( m[1][0] * A1323 - m[1][1] * A0323 + m[1][3] * A0123 )
+                   - m[0][3] * ( m[1][0] * A1223 - m[1][1] * A0223 + m[1][2] * A0123 );
+
+    if (det == 0.0) return T();
+
+    det = 1.0 / det;
+
+    return T(
+        det *   ( m[1][1] * A2323 - m[1][2] * A1323 + m[1][3] * A1223 ), // 00
+        det * - ( m[0][1] * A2323 - m[0][2] * A1323 + m[0][3] * A1223 ), // 01
+        det *   ( m[0][1] * A2313 - m[0][2] * A1313 + m[0][3] * A1213 ), // 02
+        det * - ( m[0][1] * A2312 - m[0][2] * A1312 + m[0][3] * A1212 ), // 03
+        det * - ( m[1][0] * A2323 - m[1][2] * A0323 + m[1][3] * A0223 ), // 10
+        det *   ( m[0][0] * A2323 - m[0][2] * A0323 + m[0][3] * A0223 ), // 11
+        det * - ( m[0][0] * A2313 - m[0][2] * A0313 + m[0][3] * A0213 ), // 12
+        det *   ( m[0][0] * A2312 - m[0][2] * A0312 + m[0][3] * A0212 ), // 13
+        det *   ( m[1][0] * A1323 - m[1][1] * A0323 + m[1][3] * A0123 ), // 20
+        det * - ( m[0][0] * A1323 - m[0][1] * A0323 + m[0][3] * A0123 ), // 21
+        det *   ( m[0][0] * A1313 - m[0][1] * A0313 + m[0][3] * A0113 ), // 22
+        det * - ( m[0][0] * A1312 - m[0][1] * A0312 + m[0][3] * A0112 ), // 23
+        det * - ( m[1][0] * A1223 - m[1][1] * A0223 + m[1][2] * A0123 ), // 30
+        det *   ( m[0][0] * A1223 - m[0][1] * A0223 + m[0][2] * A0123 ), // 31
+        det * - ( m[0][0] * A1213 - m[0][1] * A0213 + m[0][2] * A0113 ), // 32
+        det *   ( m[0][0] * A1212 - m[0][1] * A0212 + m[0][2] * A0112)); // 33
+}
+
+mat4 vsg::inverse(const mat4& m)
+{
+    return t_inverse(m);
+}
+
+dmat4 vsg::inverse(const dmat4& m)
+{
+    return t_inverse(m);
+}
