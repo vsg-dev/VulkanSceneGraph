09800ec79af53e0a794cf7a0648703d668830fab
Robert Osfield <robert@openscenegraph.com>
2020-01-17 11:55:20 +0000

    Removed comment

--- src/vsg/vk/Context.cpp
+++ src/vsg/vk/Context.cpp
@@ -652,7 +652,7 @@ void Context::dispatch()
     ref_ptr<DeviceMemory> scratchBufferMemory;
     if (scratchBufferSize > 0)
     {
-        scratchBuffer = Buffer::create(device, scratchBufferSize, VK_BUFFER_USAGE_RAY_TRACING_BIT_NV, VK_SHARING_MODE_EXCLUSIVE); // RAYTRACING HACK
+        scratchBuffer = Buffer::create(device, scratchBufferSize, VK_BUFFER_USAGE_RAY_TRACING_BIT_NV, VK_SHARING_MODE_EXCLUSIVE);
 
         scratchBufferMemory = vsg::DeviceMemory::create(device, scratchBuffer, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
         scratchBuffer->bind(scratchBufferMemory, 0);
