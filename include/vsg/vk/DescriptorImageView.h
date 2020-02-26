5d951cc846f97a5e4ddd93eb2f852fb330280105
Robert Osfield <robert@openscenegraph.com>
2020-02-26 12:17:17 +0000

    Removed duplicates

--- include/vsg/vk/DescriptorImageView.h
+++ include/vsg/vk/DescriptorImageView.h
@@ -17,55 +17,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 namespace vsg
 {
-    struct SamplerImage
-    {
-        ref_ptr<Sampler> sampler;
-        ref_ptr<Data> data;
-        ref_ptr<ImageView> imageView;
-    };
-    using SamplerImages = std::vector<SamplerImage>;
-
-    class VSG_DECLSPEC DescriptorImage : public Inherit<Descriptor, DescriptorImage>
-    {
-    public:
-        DescriptorImage();
-
-        DescriptorImage(ref_ptr<Sampler> sampler, ref_ptr<Data> image, uint32_t dstBinding = 0, uint32_t dstArrayElement = 0, VkDescriptorType descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
-
-        template<class T>
-        DescriptorImage(ref_ptr<Sampler> sampler, ref_ptr<T> image, uint32_t dstBinding = 0, uint32_t dstArrayElement = 0, VkDescriptorType descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER) :
-            DescriptorImage(sampler, ref_ptr<Data>(image), dstBinding, dstArrayElement, descriptorType) {}
-
-        DescriptorImage(ref_ptr<Sampler> sampler, ref_ptr<ImageView> image, uint32_t dstBinding = 0, uint32_t dstArrayElement = 0, VkDescriptorType descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
-
-        DescriptorImage(const SamplerImage& samplerImage, uint32_t dstBinding = 0, uint32_t dstArrayElement = 0, VkDescriptorType descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
-
-        DescriptorImage(const SamplerImages& samplerImages, uint32_t dstBinding = 0, uint32_t dstArrayElement = 0, VkDescriptorType descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
-
-        SamplerImages& getSamplerImages() { return _samplerImages; }
-        const SamplerImages& getSamplerImages() const { return _samplerImages; }
-
-        /** ImageDataList is automatically filled in by the DecriptorImage::compile() using the sampler and image data objects.*/
-        ImageDataList& getImageDataList() { return _imageDataList; }
-        const ImageDataList& getImageDataList() const { return _imageDataList; }
-
-        void read(Input& input) override;
-        void write(Output& output) const override;
-
-        void compile(Context& context) override;
-
-        void assignTo(Context& context, VkWriteDescriptorSet& wds) const override;
-
-        uint32_t getNumDescriptors() const override;
-
-    protected:
-        SamplerImages _samplerImages;
-
-        // populated by compile()
-        // TODO : per Device?
-        ImageDataList _imageDataList;
-    };
-    VSG_type_name(vsg::DescriptorImage);
 
     class VSG_DECLSPEC DescriptorImageView : public Inherit<Descriptor, DescriptorImageView>
     {
