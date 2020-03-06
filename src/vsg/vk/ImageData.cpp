36c3508c9531f737e92700addde1b5990da6ee80
Robert Osfield <robert@openscenegraph.com>
2020-03-06 10:25:38 +0000

    Added TODO as a reminder that the automatic selection of VkImageType/VkImageViewType isn't always appropriate

--- src/vsg/vk/ImageData.cpp
+++ src/vsg/vk/ImageData.cpp
@@ -115,6 +115,7 @@ ImageData vsg::transferImageData(Context& context, const Data* data, Sampler* sa
     uint32_t height = data->height() * layout.blockHeight;
     uint32_t depth = data->depth() * layout.blockDepth;
 
+    // TODO, need to look at better user control of the VkImageType/VkImageViewType as automatic selection looks to be problematic when the dimensions are 1D but the shader is expecting 2D.
     VkImageType imageType = depth > 1 ? VK_IMAGE_TYPE_3D : (height > 1 ? VK_IMAGE_TYPE_2D : VK_IMAGE_TYPE_1D);
     VkImageViewType imageViewType = depth > 1 ? VK_IMAGE_VIEW_TYPE_3D : (height > 1 ? VK_IMAGE_VIEW_TYPE_2D : VK_IMAGE_VIEW_TYPE_1D);
 
