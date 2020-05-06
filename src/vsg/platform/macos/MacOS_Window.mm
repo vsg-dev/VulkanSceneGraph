ca589875f6fefc2a7b33723975c78457f3876726
Robert Osfield <robert@openscenegraph.com>
2020-05-06 17:20:21 +0100

    Added Exception.h include

--- src/vsg/platform/macos/MacOS_Window.mm
+++ src/vsg/platform/macos/MacOS_Window.mm
@@ -12,6 +12,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #include <vsg/platform/macos/MacOS_Window.h>
 
+#include <vsg/core/Exception.h>
 #include <vsg/core/observer_ptr.h>
 #include <vsg/ui/KeyEvent.h>
 #include <vsg/ui/PointerEvent.h>
