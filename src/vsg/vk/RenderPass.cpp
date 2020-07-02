2e93bcabecd9086b509561b495271420afd38a63
Robert Osfield <robert@openscenegraph.com>
2020-07-02 14:41:32 +0100

    Changed default back to VK_ATTACHMENT_STORE_OP_DONT_CARE for efficiency

--- src/vsg/vk/RenderPass.cpp
+++ src/vsg/vk/RenderPass.cpp
@@ -91,7 +91,7 @@ AttachmentDescription vsg::defaultDepthAttachment(VkFormat depthFormat)
     depthAttachment.format = depthFormat;
     depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
     depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-    depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+    depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
     depthAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
     depthAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
     depthAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
