6b1bf69814764f4f4bd85c62a9a3575c0a753990
Robert Osfield <robert@openscenegraph.com>
2020-07-15 13:11:04 +0100

    Added exports to quieten Windows dll warnings

--- include/vsg/state/GraphicsPipeline.h
+++ include/vsg/state/GraphicsPipeline.h
@@ -20,7 +20,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 namespace vsg
 {
 
-    class GraphicsPipelineState : public Inherit<Object, GraphicsPipelineState>
+    class VSG_DECLSPEC GraphicsPipelineState : public Inherit<Object, GraphicsPipelineState>
     {
     public:
         GraphicsPipelineState() {}
