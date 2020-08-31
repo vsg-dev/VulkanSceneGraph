4d519ea95718a2ecfbc81315e1ed367060bde284
Robert Osfield <robert.osfield@gmail.com>
2020-08-31 10:52:46 +0100

    Added missing os_copilatible implementations

--- include/vsg/core/Array3D.h
+++ include/vsg/core/Array3D.h
@@ -86,6 +86,7 @@ namespace vsg
         std::size_t sizeofObject() const noexcept override { return sizeof(Array3D); }
         const char* className() const noexcept override { return type_name<Array3D>(); }
         const std::type_info& type_info() const noexcept override { return typeid(*this); }
+        bool is_compatible(const std::type_info& type) const noexcept override { return typeid(Array3D) == type ? true : Data::is_compatible(type); }
 
         // implementation provided by Visitor.h
         void accept(Visitor& visitor) override;
