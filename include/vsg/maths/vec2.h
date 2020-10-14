a4faddd266266aeb5e14e7adf3f5ff49ac06ad59
Robert Osfield <robert@openscenegraph.com>
2020-10-14 10:45:52 +0100

    Added vec2::cross() function and fixed vsg2::dot() function.

--- include/vsg/maths/vec2.h
+++ include/vsg/maths/vec2.h
@@ -207,7 +207,15 @@ namespace vsg
     template<typename T>
     constexpr T dot(const t_vec2<T>& lhs, const t_vec2<T>& rhs)
     {
-        return lhs[0] * rhs[0] + lhs[1] * rhs[1] + lhs[2] * rhs[2];
+        return lhs[0] * rhs[0] + lhs[1] * rhs[1];
+    }
+
+    /// cross product of a vec2 can be thought of cross product of vec3's with the z value of 0.0/vec3's in the xy plane.
+    /// The retuned value is the length of the resulting vec3 cross product, and can be treated as the signed area of the parallelogram, negative if rhs is clockwise from lhs when looking down on xy plane.
+    template<typename T>
+    constexpr T cross(const t_vec2<T>& lhs, const t_vec2<T>& rhs)
+    {
+        return (lhs[0] * rhs[1] - rhs[0] * lhs[1]);
     }
 
 } // namespace vsg
