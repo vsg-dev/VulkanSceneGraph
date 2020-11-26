1124c83bfc8e8c6279305231f8f361cb7b922eb3
Robert Osfield <robert@openscenegraph.com>
2020-11-26 15:28:21 +0000

    Added new Cpu and GpuLayoutTechnique

--- src/vsg/io/ObjectFactory.cpp
+++ src/vsg/io/ObjectFactory.cpp
@@ -192,6 +192,8 @@ ObjectFactory::ObjectFactory()
     VSG_REGISTER_create(vsg::Font);
     VSG_REGISTER_create(vsg::Text);
     VSG_REGISTER_create(vsg::LeftAlignment);
+    VSG_REGISTER_create(vsg::CpuLayoutTechnique);
+    VSG_REGISTER_create(vsg::GpuLayoutTechnique);
 
     // application
     VSG_REGISTER_create(vsg::EllipsoidModel);
