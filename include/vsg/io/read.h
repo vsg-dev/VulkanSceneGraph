b6eabeb938841ba52395c05132b83ca0cbab2e8f
Andre Normann <andre@fam-normann.de>
2021-01-19 08:06:44 +0100

    Added stream support to vsg::read and vsg::read_cast

--- include/vsg/io/read.h
+++ include/vsg/io/read.h
@@ -15,6 +15,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <vsg/core/Inherit.h>
 #include <vsg/io/FileSystem.h>
 #include <vsg/io/Options.h>
+#include <istream>
 
 namespace vsg
 {
@@ -25,11 +26,22 @@ namespace vsg
     /** convenience method for reading objects from files.*/
     extern VSG_DECLSPEC PathObjects read(const Paths& filenames, ref_ptr<const Options> options = {});
 
+    /** convenience method for reading objects from stream.*/
+    extern VSG_DECLSPEC ref_ptr<Object> read(std::istream& fin, ref_ptr<const Options> options = {});
+
     /** convenience method for reading file with cast to specified type.*/
     template<class T>
     ref_ptr<T> read_cast(const Path& filename, ref_ptr<const Options> options = {})
     {
         auto object = read(filename, options);
+        return vsg::ref_ptr<T>(dynamic_cast<T*>(object.get()));
+    }
+
+    /** convenience method for reading stream with cast to specified type.*/
+    template<class T>
+    ref_ptr<T> read_cast(std::istream& fin, ref_ptr<const Options> options = {})
+    {
+        auto object = read(fin, options);
         return vsg::ref_ptr<T>(dynamic_cast<T*>(object.get()));
     }
 
