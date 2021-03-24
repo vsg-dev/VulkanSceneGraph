fa90a7fbae69ce2b7f060008edddb3cd58065260
Robert Osfield <robert@openscenegraph.com>
2021-03-24 12:02:02 +0000

    Added smoothstep(..) functions

--- include/vsg/maths/transform.h
+++ include/vsg/maths/transform.h
@@ -32,6 +32,25 @@ namespace vsg
     constexpr float degrees(float radians) noexcept { return radians * (180.0f / PIf); }
     constexpr double degrees(double radians) noexcept { return radians * (180.0 / PI); }
 
+    /// Hermite interoplation between edge0 and edge1
+    template<typename T>
+    T smoothstep(T edge0, T edge1, T x)
+    {
+        if (x<=edge0) return edge0;
+        else if (x>=edge1) return edge1;
+        double r = (x-edge0)/(edge1-edge0);
+        return edge0 + (r * r * (3.0 - 2.0 * r)) * (edge1-edge0);
+    }
+
+    /// Hermite interoplation between 0.0 and 1.0
+    template<typename T>
+    T smoothstep(T r)
+    {
+        if (r<=0.0) return 0.0;
+        else if (r>=1.0) return 1.0;
+        return r * r * (3.0 - 2.0 * r);
+    }
+
     template<typename T>
     t_mat4<T> rotate(T angle_radians, T x, T y, T z)
     {
