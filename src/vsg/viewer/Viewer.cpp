805b253b4ac71272927551630eb07b780fc284f0
Robert Osfield <robert@openscenegraph.com>
2021-05-14 10:17:59 +0100

    Added overrideMask setting for CompileTravesal to ensure it traverses the all parts of the scene graph.

--- src/vsg/viewer/Viewer.cpp
+++ src/vsg/viewer/Viewer.cpp
@@ -246,8 +246,10 @@ void Viewer::compile(BufferPreferences bufferPreferences)
         auto queueFamily = physicalDevice->getQueueFamily(VK_QUEUE_GRAPHICS_BIT); // TODO : could we just use transfer bit?
 
         deviceResource.compile = new vsg::CompileTraversal(device, bufferPreferences);
+        deviceResource.compile->overrideMask = 0xffffffff;
         deviceResource.compile->context.commandPool = vsg::CommandPool::create(device, queueFamily, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
         deviceResource.compile->context.graphicsQueue = device->getQueue(queueFamily);
+
 
         if (descriptorPoolSizes.size() > 0) deviceResource.compile->context.descriptorPool = vsg::DescriptorPool::create(device, maxSets, descriptorPoolSizes);
     }
