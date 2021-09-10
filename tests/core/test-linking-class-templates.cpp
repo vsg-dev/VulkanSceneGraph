485f4a020f0af7e0f043ae6ac027b2c25c7fe646
Ralf Habacker <ralf.habacker@freenet.de>
2021-09-10 09:05:06 +0200

    Add test case for linking to class templates

---
+++ tests/core/test-linking-class-templates.cpp
@@ -0,0 +1,15 @@
+
+#include <vsg/core/Array.h>
+#include <vsg/core/Array2D.h>
+#include <vsg/core/Array3D.h>
+#include <vsg/core/Value.h>
+
+int main()
+{
+    vsg::ref_ptr<vsg::Array<uint8_t>> a = vsg::ubyteArray::create(1024);
+    vsg::ref_ptr<vsg::Array2D<uint8_t>> a2d = vsg::ubyteArray2D::create(1, 2);
+    vsg::ref_ptr<vsg::Array3D<uint8_t>> a3d = vsg::ubyteArray3D::create(1, 2, 3);
+    vsg::ref_ptr<vsg::Value<unsigned int>> v = vsg::uintValue::create(0);
+
+    return 0;
+}
