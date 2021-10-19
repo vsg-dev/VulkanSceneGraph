7a9cb6f25e8a455dcb2465e10c18583b38f1c479
Robert Osfield <robert@openscenegraph.com>
2021-10-19 14:11:29 +0100

    Added vsg::AnimationPath to help with animations

---
+++ src/vsg/utils/AnimationPath.cpp
@@ -0,0 +1,55 @@
+/* <editor-fold desc="MIT License">
+
+Copyright(c) 2021 Robert Osfield
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+</editor-fold> */
+
+#include <vsg/utils/AnimationPath.h>
+
+using namespace vsg;
+
+AnimationPath::Location AnimationPath::computeLocation(double time) const
+{
+    // check for empy locations map
+    if (locations.empty()) return {};
+
+    // check for single entry in locations map
+    if (locations.begin() == locations.rbegin().base()) return locations.begin()->second;
+
+    if (mode == REPEAT)
+    {
+        double period = locations.rbegin()->first - locations.begin()->first;
+        time = locations.begin()->first + std::fmod(time - locations.begin()->first, period);
+    }
+    else if (mode == FORWARD_AND_BACK)
+    {
+        double period = locations.rbegin()->first - locations.begin()->first;
+        double t = std::fmod(time - locations.begin()->first, period * 2.0);
+        if (t <= period)
+            time = locations.begin()->first + t;
+        else if (t > period)
+            time = locations.begin()->first + period * 2.0 - t;
+    }
+
+    if (time <= locations.begin()->first) return locations.begin()->second;
+    if (time >= locations.rbegin()->first) return locations.rbegin()->second;
+
+    auto not_less_itr = locations.lower_bound(time);
+    if (not_less_itr == locations.end()) return {};
+    if (not_less_itr == locations.begin()) return not_less_itr->second;
+
+    auto less_than_itr = not_less_itr;
+    --not_less_itr;
+
+    auto& lower = less_than_itr->second;
+    auto& upper = not_less_itr->second;
+    double r = (time - less_than_itr->first) / (not_less_itr->first - less_than_itr->first);
+
+    return Location{mix(lower.position, upper.position, r), mix(lower.orientation, upper.orientation, r)};
+}
