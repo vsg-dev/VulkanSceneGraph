cdf229943d2a9b07c1626f8fc81926ffcaa6e0ca
Robert Osfield <robert@openscenegraph.com>
2021-11-11 14:17:57 +0000

    Moved the ComputeTransform visitor out of tempalte into .cpp

--- include/vsg/maths/transform.h
+++ include/vsg/maths/transform.h
@@ -12,12 +12,11 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 </editor-fold> */
 
-#include <vsg/core/Export.h>
+#include <vsg/core/ConstVisitor.h>
 #include <vsg/maths/mat3.h>
 #include <vsg/maths/mat4.h>
 #include <vsg/maths/quat.h>
 #include <vsg/maths/vec3.h>
-#include <vsg/nodes/MatrixTransform.h>
 
 #include <cmath>
 
@@ -220,25 +219,18 @@ namespace vsg
     /// compute the bounding sphere that encloses a frustum defined by specified double ModelViewMatrixProjection
     extern VSG_DECLSPEC dsphere computeFrustumBound(const dmat4& m);
 
+    struct ComputeTransform : public ConstVisitor
+    {
+        dmat4 matrix;
+
+        void apply(const Transform& transform) override;
+        void apply(const MatrixTransform& mt) override;
+    };
+
     // convinience function for accumulating the transforms in scene graph along a specified nodePath.
     template<typename T>
     dmat4 computeTransform(const T& nodePath)
     {
-        struct ComputeTransform : public ConstVisitor
-        {
-            dmat4 matrix;
-
-            void apply(const Transform& transform) override
-            {
-                matrix = transform.transform(matrix);
-            }
-
-            void apply(const MatrixTransform& mt) override
-            {
-                matrix = matrix * mt.matrix;
-            }
-        };
-
         ComputeTransform ct;
         for (auto& node : nodePath)
         {
