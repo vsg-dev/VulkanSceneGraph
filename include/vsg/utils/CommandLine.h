5449336303ab42394ce614281433d3b7ed1502c8
Robert Osfield <robert@openscenegraph.com>
2021-12-01 14:24:15 +0000

    Added Options.h include

--- include/vsg/utils/CommandLine.h
+++ include/vsg/utils/CommandLine.h
@@ -15,6 +15,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <vsg/core/Export.h>
 #include <vsg/core/type_name.h>
 #include <vsg/io/stream.h>
+#include <vsg/io/Options.h>
 
 #include <vector>
 
