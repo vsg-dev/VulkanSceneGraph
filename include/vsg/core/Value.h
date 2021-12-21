6c5c5feb33a7625738b051906bfebde96ef1d578
Roland Hill <roleyhill@gmail.com>
2021-12-22 09:27:59 +1100

    Revert "Add vsg:: prefix to VSG_value define"
    This reverts commit 1775636c170a3264b18bb46c701423ff21815725.

--- include/vsg/core/Value.h
+++ include/vsg/core/Value.h
@@ -28,7 +28,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <vsg/io/Output.h>
 
 #define VSG_value(N, T) \
-    using N = vsg::Value<T>; \
+    using N = Value<T>; \
     template<>          \
     constexpr const char* type_name<N>() noexcept { return "vsg::" #N; }
 
