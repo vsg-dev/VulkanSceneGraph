b93ba6cd413fc4a8ea2d63cd789f979502249e4a
Robert Osfield <robert@openscenegraph.com>
2022-01-11 10:31:01 +0000

    Added missing export

--- include/vsg/maths/transform.h
+++ include/vsg/maths/transform.h
@@ -222,7 +222,7 @@ namespace vsg
 
     /// visitor that computes a transform matrix, accumulating the result in order of objects visited
     /// usage:  auto matrix = vsg::visit<vsg::ComputeTransform>(nodePath).matrix;
-    struct ComputeTransform : public ConstVisitor
+    struct VSG_DECLSPEC ComputeTransform : public ConstVisitor
     {
         dmat4 matrix;
 
