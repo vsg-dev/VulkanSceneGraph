35ce23d4692cb9e149aff973ed20c8261f30176d
Robert Osfield <robert@openscenegraph.com>
2022-05-04 18:26:08 +0100

    Added initial support for using vsg::wstring and vsg::string versions of vsg::Path.

--- include/vsg/io/Path.h
+++ include/vsg/io/Path.h
@@ -18,23 +18,33 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <ostream>
 
 #define NEW_PATH_DEFINED 1
+#define WIDE_PATH 0
 
 #include <iostream>
 
 namespace vsg
 {
 
+    extern VSG_DECLSPEC void copy(const std::string& src, std::wstring& dst);
+    extern VSG_DECLSPEC void copy(const std::wstring& src, std::string& dst);
+
+    inline void copy(const std::string& src, std::string& dst) { dst = src; }
+    inline void copy(const std::wstring& src, std::wstring& dst) { dst = src; }
+    inline void copy(const char c, std::string& dst) { dst.clear(); dst.push_back(c); }
+    inline void copy(const char c, std::wstring& dst) { dst.clear(); dst.push_back(static_cast<wchar_t>(c)); }
+
 #if NEW_PATH_DEFINED
 
     class VSG_DECLSPEC Path
     {
     public:
-        Path() {}
-        Path(const Path& path) : _string(path._string) {}
-        Path(const std::string& str) : _string(str) {}
-        Path(const char* str) : _string(str) {}
 
+#if WIDE_PATH
+        using value_type = wchar_t;
+#else
         using value_type = char;
+#endif
+
         using string_type = std::basic_string<value_type>;
 
         using size_type = size_t;
@@ -47,59 +57,75 @@ namespace vsg
 
         static const size_type npos = static_cast<size_type>(-1);
 
+        Path();
+        Path(const Path& path);
+        Path(const char* str);
+        Path(const std::string& str);
+        Path(const std::wstring& str);
+
+        void assign(const std::string& str) { copy(str, _string); }
+        void assign(const char* str) { copy(str, _string); }
+        void assign(const std::wstring& str) { copy(str, _string); }
+
+        inline string_type native(const std::string& str) const { string_type dest; copy(str, dest); return dest; }
+        inline string_type native(const std::wstring& str) const { string_type dest; copy(str, dest); return dest; }
+        inline string_type native(const char* str) const { string_type dest; copy(str, dest); return dest; }
+        inline string_type native(const char c) const { string_type dest; copy(c, dest); return dest; }
+
         iterator begin() { return _string.begin(); }
         iterator end() { return _string.end(); }
         const_iterator begin() const { return _string.begin(); }
         const_iterator end() const  { return _string.end(); }
 
         int compare(const Path& rhs) const { return _string.compare(rhs._string); }
-        int compare(const char* rhs) const { return _string.compare(rhs); }
         int compare(size_type pos, size_type n, const Path& rhs) const { return _string.compare(pos, n, rhs._string); }
-        int compare(size_type pos, size_type n, const char* rhs) const { return _string.compare(pos, n, rhs); }
+
+        int compare(const char* rhs) const { return _string.compare(native(rhs)); }
+        int compare(size_type pos, size_type n, const char* rhs) const { return _string.compare(pos, n, native(rhs)); }
 
         Path& operator = (const Path& path) {if (this != &path) _string = path._string; return *this; }
-        Path& operator = (const std::string& str) { _string = str; return *this; }
-        Path& operator = (const char* str) { _string = str; return *this; }
+        Path& operator = (const std::string& str) { assign(str); return *this; }
+        Path& operator = (const char* str) { assign(str); return *this; }
 
         bool operator == (const Path& rhs) const { return compare(rhs) == 0; }
-        bool operator == (const char* rhs) const { return compare(rhs) == 0; }
         bool operator != (const Path& rhs) const { return compare(rhs) != 0; }
-        bool operator != (const char* rhs) const { return compare(rhs) != 0; }
         bool operator < (const Path& rhs) const { return compare(rhs) < 0; }
+
+        bool operator == (const char* rhs) const { return compare(native(rhs)) == 0; }
+        bool operator != (const char* rhs) const { return compare(native(rhs)) != 0; }
 
         bool empty() const { return _string.empty(); }
         size_type size() const { return _string.size(); }
         size_type length() const { return _string.size(); }
 
-        const std::string& string() const { return _string; }
-        const char* c_str() const { return _string.c_str(); }
+        inline std::string string() const { std::string dest; copy(_string, dest); return dest; }
+        inline std::wstring wstring() const { std::wstring dest; copy(_string, dest); return dest; }
 
         reference operator [] (size_type pos) { return _string[pos]; }
         const_reference operator [] (size_type pos) const { return _string[pos]; }
 
-        //operator const char* () const { return _string.c_str(); }
         operator const string_type& () const { return _string; }
 
         Path substr(size_type pos, size_type len = Path::npos) const { return Path(_string.substr(pos, len)); }
 
         size_type find(const Path& s, size_type pos = 0) const { return _string.find(s._string, pos); }
-        size_type find(const char* s, size_type pos = 0) const { return _string.find(s, pos); }
+        size_type find(const char* s, size_type pos = 0) const { return _string.find(native(s), pos); }
 
         size_type find_first_of(const Path& s, size_type pos = 0) const { return _string.find_first_of(s._string, pos); }
-        size_type find_first_of(const char* s, size_type pos = 0) const { return _string.find_first_of(s, pos); }
-        size_type find_first_of(const char c, size_type pos = 0) const { return _string.find_first_of(c, pos); }
+        size_type find_first_of(const char* s, size_type pos = 0) const { return find_first_of(native(s), pos); }
+        size_type find_first_of(const char c, size_type pos = 0) const { return find_first_of(native(c), pos); }
 
         size_type find_last_of(const Path& s, size_type pos = npos) const { return _string.find_last_of(s._string, pos); }
-        size_type find_last_of(const char* s, size_type pos = npos) const { return _string.find_last_of(s, pos); }
-        size_type find_last_of(const char c, size_type pos = npos) const { return _string.find_last_of(c, pos); }
+        size_type find_last_of(const char* s, size_type pos = npos) const { return find_last_of(native(s), pos); }
+        size_type find_last_of(const char c, size_type pos = npos) const { return find_last_of(native(c), pos); }
 
         void append(const Path& path) { _string.append(path._string); }
         void append(char c) { _string.push_back(c); }
 
         Path& replace(size_type pos, size_type n, const Path& str) { _string.replace(pos, n, str._string); return *this; }
-        Path& replace(size_type pos, size_type n, const std::string& str) { _string.replace(pos, n, str); return *this; }
-        Path& replace(size_type pos, size_type n, const char* str) {  _string.replace(pos, n, str); return *this; }
-
+        Path& replace(size_type pos, size_type n, const std::string& str) { _string.replace(pos, n, native(str)); return *this; }
+        Path& replace(size_type pos, size_type n, const std::wstring& str) { _string.replace(pos, n, native(str)); return *this; }
+        Path& replace(size_type pos, size_type n, const char* str) {  _string.replace(pos, n, native(str)); return *this; }
 
     protected:
         string_type _string = {};
@@ -133,6 +159,7 @@ namespace vsg
         path = str;
         return input;
     }
+
 #else
     using Path = std::string;
 #endif
--- src/vsg/io/Path.cpp
+++ src/vsg/io/Path.cpp
@@ -12,6 +12,61 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 #include <vsg/io/Options.h>
 
-
 using namespace vsg;
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// copy between UTF9 <-> UF16
+//
+// https://en.wikipedia.org/wiki/UTF-8
+// https://en.wikipedia.org/wiki/UTF-16
+// OpenSceneGraph/src/osgText/String.cpp
+void vsg::copy(const std::string& src, std::wstring& dst)
+{
+    dst.clear();
+    std::cout<<"TODO convert from utf8 to utf16"<<std::endl;
+    for(auto itr = src.begin(); itr != src.end();)
+    {
+        uint32_t c0 = *itr++;
+        dst.push_back(static_cast<wchar_t>(c0));
+    }
+}
+
+void vsg::copy(const std::wstring& src, std::string& dst)
+{
+    std::cout<<"TODO convert from utf16 to utf8"<<std::endl;
+    for(auto itr = src.begin(); itr != src.end();)
+    {
+        uint32_t c0 = *itr++;
+        dst.push_back(static_cast<char>(c0));
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Path
+//
+// https://en.cppreference.com/w/cpp/filesystem/path
+Path::Path()
+{
+}
+
+Path::Path(const Path& path) :
+    _string(path._string)
+{
+}
+
+Path::Path(const char* str)
+{
+    assign(str);
+}
+
+Path::Path(const std::string& str)
+{
+    assign(str);
+}
+
+Path::Path(const std::wstring& str)
+{
+    assign(str);
+}
