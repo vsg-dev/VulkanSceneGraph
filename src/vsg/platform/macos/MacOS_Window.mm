6cee5199f2507e3e31a18993dd1c3bd43aeac19a
Robert Osfield <robert@openscenegraph.com>
2022-12-09 08:40:51 +0000

    Commented out deprecated setting.

--- src/vsg/platform/macos/MacOS_Window.mm
+++ src/vsg/platform/macos/MacOS_Window.mm
@@ -800,7 +800,7 @@ MacOS_Window::MacOS_Window(vsg::ref_ptr<vsg::WindowTraits> traits) :
     
     // create view
     _view = [[vsg_MacOS_NSView alloc] initWithVsgWindow:this];
-    [_view setWantsBestResolutionOpenGLSurface:_traits->hdpi];
+    // [_view setWantsBestResolutionOpenGLSurface:_traits->hdpi];
     [_view setAutoresizingMask: (NSViewWidthSizable | NSViewHeightSizable) ];
     [_view setWantsLayer:YES];
 
