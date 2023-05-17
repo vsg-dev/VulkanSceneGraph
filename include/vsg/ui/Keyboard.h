d7dbbb82287912478f7433539e6de6313e3ef8e7
Robert Osfield <robert@openscenegraph.com>
2023-05-17 18:26:18 +0100

    Added const

--- include/vsg/ui/Keyboard.h
+++ include/vsg/ui/Keyboard.h
@@ -38,11 +38,11 @@ namespace vsg
         std::map<KeySymbol, KeyHistory> keyState;
 
         /// return true if key is currently pressed
-        bool pressed(KeySymbol key, bool ignore_handled_keys = true);
+        bool pressed(KeySymbol key, bool ignore_handled_keys = true) const;
 
         /// return a pair of times, the first is the time, in seconds, since the key was first pressed and the second is the time, in secnds, since it was released.
         /// if the key hasn't been pressed then then first value will be < 0.0, if the key is still pressed then the second value will be 0.0.
-        std::pair<double, double> times(KeySymbol key, bool ignore_handled_keys = true);
+        std::pair<double, double> times(KeySymbol key, bool ignore_handled_keys = true) const;
     };
     VSG_type_name(vsg::Keyboard);
 
