6325d4e50c92919836ff7ee9407edfd24ce30dc9
hpxl <jamierobertson1@hotmail.com>
2023-05-27 21:26:15 +0100

    Update Win32_Window.cpp
    Ignore repeated key down events in windows generated when holding keys down for a long durationsby checking  whether the previous key state was down (bit 30 in lParam in WM_KEYDOWN events).

--- src/vsg/platform/win32/Win32_Window.cpp
+++ src/vsg/platform/win32/Win32_Window.cpp
@@ -616,7 +616,9 @@ LRESULT Win32_Window::handleWin32Messages(UINT msg, WPARAM wParam, LPARAM lParam
         if (_keyboard->getKeySymbol(wParam, lParam, keySymbol, modifiedKeySymbol, keyModifier))
         {
             int32_t repeatCount = (lParam & 0xffff);
-            bufferedEvents.emplace_back(vsg::KeyPressEvent::create(this, event_time, keySymbol, modifiedKeySymbol, keyModifier, repeatCount));
+            bool previousKeyDown = (lParam & 0x40000000);
+            if (!previousKeyDown)
+                bufferedEvents.emplace_back(vsg::KeyPressEvent::create(this, event_time, keySymbol, modifiedKeySymbol, keyModifier, repeatCount));
         }
         break;
     }
