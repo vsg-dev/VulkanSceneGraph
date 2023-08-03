90c2ffb5418938ba2c41d66c1175bc6232a7fe36
Robert Osfield <robert@openscenegraph.com>
2023-08-03 16:51:48 +0100

    Build fix

--- include/vsg/state/ViewDependentState.h
+++ include/vsg/state/ViewDependentState.h
@@ -15,6 +15,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <vsg/nodes/Light.h>
 #include <vsg/state/BindDescriptorSet.h>
 #include <vsg/state/DescriptorBuffer.h>
+#include <vsg/state/DescriptorImage.h>
 
 namespace vsg
 {
