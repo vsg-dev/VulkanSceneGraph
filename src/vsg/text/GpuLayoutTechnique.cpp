8cdf872510de727797396f57796e60776e07a3d5
LeeRiva <76054616+LeeRiva@users.noreply.github.com>
2023-08-17 22:12:38 +0200

    Update GpuLayoutTechnique.cpp
    Checking that the text input has a valid (ie non-nullptr) font member and bail out if that is not the case

--- src/vsg/text/GpuLayoutTechnique.cpp
+++ src/vsg/text/GpuLayoutTechnique.cpp
@@ -44,10 +44,10 @@ void assignValue(T& dest, const T& src, bool& updated)
 
 void GpuLayoutTechnique::setup(Text* text, uint32_t minimumAllocation, ref_ptr<const Options> options)
 {
-    if (!text)
-        return;
     auto layout = text->layout;
     if (!layout) return;
+
+    if (!text->font) return;
 
     textExtents = layout->extents(text->text, *(text->font));
 
