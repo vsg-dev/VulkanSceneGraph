ea899d35169f1c522ee7fe72adb31c4261c46311
Robert Osfield <robert@openscenegraph.com>
2023-12-21 16:00:07 +0000

    Refined colours

--- include/vsg/utils/Instrumentation.h
+++ include/vsg/utils/Instrumentation.h
@@ -134,14 +134,14 @@ namespace vsg
     };
 
 // standard colours specified in {r, g, b, a} ordering
-#define COLOR_VIEWER uint_color(128, 249, 240, 255)
+#define COLOR_VIEWER uint_color(127, 240, 240, 255)
 #define COLOR_UPDATE uint_color(0, 255, 0, 255)
-#define COLOR_RECORD uint_color(0, 255, 255, 255)
+#define COLOR_RECORD uint_color(128, 255, 0, 255)
 #define COLOR_GPU uint_color(255, 127, 0, 255)
 #define COLOR_COMPILE uint_color(255, 249, 64, 255)
-#define COLOR_READ uint_color(128, 255, 0, 255)
-#define COLOR_WRITE uint_color(255, 128, 0, 255)
 #define COLOR_PAGER uint_color(240, 255, 64, 255)
+#define COLOR_READ uint_color(0, 255, 128, 255)
+#define COLOR_WRITE uint_color(0, 128, 255, 255)
 
 #if defined(__clang__) || defined(__GNUC__)
 #    define VsgFunctionName __PRETTY_FUNCTION__
