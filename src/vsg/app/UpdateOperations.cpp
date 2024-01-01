801a31066139d8c6ae58de5ff558347dab3c126c
Jamie Robertson <jamierobertson1@hotmail.com>
2024-01-01 11:24:02 +0000

    Update UpdateOperations.cpp

--- src/vsg/app/UpdateOperations.cpp
+++ src/vsg/app/UpdateOperations.cpp
@@ -35,20 +35,8 @@ void UpdateOperations::add(ref_ptr<Operation> op, RunBehavior runBehavior)
 void UpdateOperations::remove(ref_ptr<Operation> op)
 {
     std::scoped_lock<std::mutex> lock(_updateOperationMutex);
-    for (auto itr = _updateOperationsOneTime.begin(); itr != _updateOperationsOneTime.end();)
-    {
-        if (*itr == op)
-            itr = _updateOperationsOneTime.erase(itr);
-        else
-            itr++;
-    }
-    for (auto itr = _updateOperationsAllFrames.begin(); itr != _updateOperationsAllFrames.end();)
-    {
-        if (*itr == op)
-            itr = _updateOperationsAllFrames.erase(itr);
-        else
-            itr++;
-    }
+    _updateOperationsOneTime.remove(op);
+    _updateOperationsAllFrames.remove(op);
 }
 
 void UpdateOperations::clear()
