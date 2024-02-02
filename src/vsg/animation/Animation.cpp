55d63219efc4953a615aad9967aca6b299e8190d
Robert Osfield <robert@openscenegraph.com>
2024-02-02 16:27:48 +0000

    Fixed dates

--- src/vsg/animation/Animation.cpp
+++ src/vsg/animation/Animation.cpp
@@ -1,6 +1,6 @@
 /* <editor-fold desc="MIT License">
 
-Copyright(c) 2018 Robert Osfield
+Copyright(c) 2024 Robert Osfield
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
