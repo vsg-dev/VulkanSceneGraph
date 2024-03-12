3d1645fabc7dcbab69fb22e64cb141b1c390bda5
Robert Osfield <robert@openscenegraph.com>
2024-03-12 14:31:09 +0000

    Added includes

--- include/vsg/app/RecordOperation.h
+++ include/vsg/app/RecordOperation.h
@@ -12,11 +12,9 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 </editor-fold> */
 
-#include <vsg/io/Logger.h>
-#include <vsg/threading/ActivityStatus.h>
 #include <vsg/threading/OperationQueue.h>
-
-#include <list>
+#include <vsg/app/CommandGraph.h>
+#include <vsg/io/DatabasePager.h>
 
 namespace vsg
 {
