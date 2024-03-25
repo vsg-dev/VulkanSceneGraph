22ec25855876a29822dcd19b6d90c095175e5717
Robert Osfield <robert@openscenegraph.com>
2024-03-25 19:02:20 +0000

    Fixed cppcheck issue

--- src/vsg/utils/Profiler.cpp
+++ src/vsg/utils/Profiler.cpp
@@ -142,7 +142,7 @@ Profiler::Profiler(ref_ptr<Settings> in_settings) :
 void Profiler::writeGpuTimestamp(CommandBuffer& commandBuffer, uint64_t reference, VkPipelineStageFlagBits pipelineStage) const
 {
     auto& frameStats = perFrameGPUStats[frameIndex];
-    auto& gpuStats = frameStats.perDeviceGpuStats[commandBuffer.deviceID];
+    const auto& gpuStats = frameStats.perDeviceGpuStats[commandBuffer.deviceID];
     auto index = gpuStats->queryIndex.fetch_add(1);
     if (index < gpuStats->timestamps.size())
     {
