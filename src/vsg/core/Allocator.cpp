c4771d46d7c7f7038341528b0708e77ef8003ade
Robert Osfield <robert.osfield@gmail.com>
2024-05-02 17:40:18 +0100

    Build fix

--- src/vsg/core/Allocator.cpp
+++ src/vsg/core/Allocator.cpp
@@ -15,6 +15,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <vsg/io/Logger.h>
 #include <vsg/io/Options.h>
 
+#include <cstddef>
 #include <algorithm>
 #include <iostream>
 
