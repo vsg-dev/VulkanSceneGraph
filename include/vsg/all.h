ea5e259ae3d5cf03f01acd090e2f09f36dd85d7c
LeeRiva <76054616+LeeRiva@users.noreply.github.com>
2024-07-26 15:59:55 +0200

    Added missing header

--- include/vsg/all.h
+++ include/vsg/all.h
@@ -231,6 +231,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <vsg/vk/CommandPool.h>
 #include <vsg/vk/Context.h>
 #include <vsg/vk/DescriptorPool.h>
+#include <vsg/vk/DescriptorPools.h>
 #include <vsg/vk/Device.h>
 #include <vsg/vk/DeviceExtensions.h>
 #include <vsg/vk/DeviceFeatures.h>
