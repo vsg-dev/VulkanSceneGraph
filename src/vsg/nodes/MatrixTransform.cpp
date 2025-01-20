3910ae73061d22634f8bc6ba82c68e4191659ef7
Robert Osfield <robert.osfield@gmail.com>
2025-01-20 11:52:15 +0000

    Removed no longer required includes

--- src/vsg/nodes/MatrixTransform.cpp
+++ src/vsg/nodes/MatrixTransform.cpp
@@ -11,7 +11,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 </editor-fold> */
 
 #include <vsg/core/compare.h>
-#include <vsg/io/Options.h>
 #include <vsg/io/stream.h>
 #include <vsg/nodes/MatrixTransform.h>
 
