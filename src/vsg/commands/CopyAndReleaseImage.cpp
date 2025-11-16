8d41978fe13a606a89516b4be84af2cce79273ca
Robert Osfield <robert.osfield@gmail.com>
2025-11-16 11:53:55 +0000

    Added include to fix Windows build

--- src/vsg/commands/CopyAndReleaseImage.cpp
+++ src/vsg/commands/CopyAndReleaseImage.cpp
@@ -10,10 +10,11 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 </editor-fold> */
 
+#include <vsg/app/TransferTask.h>
 #include <vsg/commands/CopyAndReleaseImage.h>
 #include <vsg/commands/PipelineBarrier.h>
 #include <vsg/io/Logger.h>
-#include <vsg/app/TransferTask.h>
+#include <vsg/utils/Instrumentation.h>
 
 using namespace vsg;
 
